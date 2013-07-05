f1e7281241eb835c708d002ca7b225a1c4e93a6d
Sheehan Olver <solver@mac.com>
2013-07-05 23:05:22 +1000

    Alternative version

--- test/Clenshaw_evaluate.m
+++ test/Clenshaw_evaluate.m
@@ -12,12 +12,12 @@ function vals = Clenshaw_evaluate(c,x)
     vals = c(end) + .5*x.*bk1 - bk2;
 end
 
-% function Clenshaw_evaluate(c::Vector{Float64},x::Vector{Float64})
-%     bk1 = zeros(length(x));
-%     bk2 = bk1;
-%     x=2*x;
-%     
-%     for k = 1:length(c)-1
+%  function Clenshaw_evaluate(c::Vector{Float64},x::Vector{Float64})
+%      bk1 = zeros(length(x));
+%      bk2 = bk1;
+%      x=2*x;
+% 
+%      for k = 1:length(c)-1
 %         bk = c[k] + x.*bk1 - bk2;
 %         bk2 = bk1;
 %         bk1 = bk;
@@ -25,13 +25,13 @@ end
 % 
 %     c[end] + .5*x.*bk1 - bk2
 % end
+% %
 % 
 % 
 % 
 % 
-
-
-
+% 
+% 
 % function clenshawv(c::Vector{Float64},x::Vector{Float64})
 %     B = zeros(length(x),length(c)+2);
 % 
