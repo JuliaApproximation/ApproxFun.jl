e46fef194a8b82b57dadbc36e288549d062d6b25
Sheehan Olver <solver@mac.com>
2013-07-07 18:33:28 +1000

    Added Laurent series

--- src/Domains.jl
+++ src/Domains.jl
@@ -1,16 +1,28 @@
 
 
-export Domain,tocanonical,fromcanonical
+export Domain,tocanonical,fromcanonical,Circle
 
 
 abstract Domain
 
 
+##General routines
+
+for op = (:tocanonical,:tocanonicalD,:fromcanonical,:fromcanonicalD)
+    @eval ($op)(f::AbstractFun,x)=($op)(f.domain,x)
+end
 
 
 
-## Interval
 
+
+
+## Interval Domains
+
+abstract IntervalDomain <: Domain
+
+chebyshevpoints(n::Integer)= cos(1.π*[n-1:-1:0]/(n-1))
+points(d::IntervalDomain,n::Integer) = fromcanonical(d, chebyshevpoints(n))
 
 
 type Interval{T<:Number} <: Domain
@@ -18,29 +30,15 @@ type Interval{T<:Number} <: Domain
 	b::T
 end
 
-type PeriodicInterval{T<:Number} <: Domain
-	a::T
-	b::T
-end
-
-Interval(d::PeriodicInterval)=Interval(d.a,d.b)
-PeriodicInterval(d::Interval)=PeriodicInterval(d.a,d.b)
 
 
 ## Points
 
-chebyshevpoints(n::Integer)= cos(1.π*[n-1:-1:0]/(n-1))
-fourierpoints(n::Integer)= 1.π*[-1.:2/n:1. - 2/n]
-
-points(d::Interval,n::Integer) = fromcanonical(d, chebyshevpoints(n))
-points(d::PeriodicInterval,n::Integer) = fromcanonical(d, fourierpoints(n))
 
 
-##Fun calls to domain calls
 
-for op = (:tocanonical,:tocanonicalD,:fromcanonical,:fromcanonicalD)
-    @eval ($op)(f::AbstractFun,x)=($op)(f.domain,x)
-end
+
+
 
 
 ## Map interval
@@ -61,13 +59,43 @@ Base.length(d::Interval) = d.b - d.a
 ==(d::Interval,m::Interval) = d.a == m.a && d.b == m.b
 
 
+
+## Arc
+
+## Polynomial map
+
+
+
+####
+## Periodic domains
+
+abstract PeriodicDomain <: Domain
+
+points(d::PeriodicDomain,n::Integer) = fromcanonical(d, fourierpoints(n))
+
+
+type PeriodicInterval{T<:Number} <: PeriodicDomain
+	a::T
+	b::T
+end
+
+
+Interval(d::PeriodicInterval)=Interval(d.a,d.b)
+PeriodicInterval(d::Interval)=PeriodicInterval(d.a,d.b)
+
+fourierpoints(n::Integer)= 1.π*[-1.:2/n:1. - 2/n]
+
+
+
+
+
 ## Map periodic interval
 
 
 tocanonical(d::PeriodicInterval,x)=1.π.*tocanonical(Interval(d),x)
 tocanonicalD(d::PeriodicInterval,x)=1.π.*tocanonicalD(Interval(d),x)
-fromcanonical(d::PeriodicInterval,x)=fromcanonical(Interval(d),x/π)
-fromcanonicalD(d::PeriodicInterval,x)=fromcanonicalD(Interval(d),x/π)/π
+fromcanonical(d::PeriodicInterval,θ)=fromcanonical(Interval(d),θ/π)
+fromcanonicalD(d::PeriodicInterval,θ)=fromcanonicalD(Interval(d),θ/π)/π
 
 
 
@@ -78,10 +106,28 @@ Base.length(d::PeriodicInterval) = d.b - d.a
 ==(d::PeriodicInterval,m::PeriodicInterval) = d.a == m.a && d.b == m.b
 
 
-## Arc
-
-## Polynomial map
+##  Circle
 
 
+type Circle <: PeriodicDomain
+	center
+	radius
+end
 
+Circle(r)=Circle(0.,r)
+Circle()=Circle(1.)
+
+
+tocanonical(d::Circle,ζ)=-1.im*log((ζ-d.center)/d.radius)
+tocanonicalD(d::Circle,ζ)=-1.im./((ζ-d.center))  #TODO: Check formula
+fromcanonical(d::Circle,θ)=d.radius*exp(1.im*θ) + d.center
+fromcanonicalD(d::Circle,θ)=d.radius*1.im*exp(1.im*θ)
+
+
+
+Base.length(d::Circle) = 2π*d.radius
+
+
+
+==(d::Circle,m::Circle) = d.center == m.center && d.radius == m.radius
 
