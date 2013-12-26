529e2a0d72e108cb61e016468ec6932a19ac2996
Sheehan Olver <solver@mac.com>
2013-12-26 21:04:46 +1100

    backslash for constant coefficient operators

--- src/Operator.jl
+++ src/Operator.jl
@@ -52,85 +52,75 @@ Base.diff(d::IntervalDomain)=Base.diff(d,1)
 
 
 
-function derivativematrix(μ::Integer,d::Domain,k::Range1,j::Range1)
-    if(μ == 0)
-        @assert k[1]==1 && j[1]==1
-    
-        speye(k[end],j[end])
+function derivativematrix(μ::Integer,d::Domain,m::Integer)
+    if(μ == 0)    
+        speye(m,m)
     else
-        ret = spzeros(size(k)[1],size(j)[1])
-        
-        st = max(k[1],j[1]-μ)
-        en = min(k[end],j[end]-μ)
-        
-        for l=st:en
-            ret[l - k[1] + 1, l - j[1] + 1 + μ] =  2.^(μ-1).*factorial(μ-1).*(2./(d.b-d.a)).^μ.*(μ+l-1)
+        ret = spzeros(m,m)
+
+        for l=1:m-μ
+            ret[l, l + μ] =  2.^(μ-1).*factorial(μ-1).*(2./(d.b-d.a)).^μ.*(μ+l-1)
         end
         
         ret
     end
 end
 
-function conversionmatrix(λ::Integer,k::Range1,j::Range1)
-    ret = spzeros(size(k)[1],size(j)[1])
+function conversionmatrix(λ::Integer,m::Integer)
+    ret = spzeros(m,m)
 
     if λ == 0
-        for l=max(k[1],j[1]):min(k[end],j[end])
-            ret[l - k[1] + 1, l - j[1] + 1] =  l==1? 1. : .5
+        for l=1:m
+            ret[l, l] =  l==1? 1. : .5
         end
         
-        for l=max(k[1],j[1]):min(k[end],j[end])-2
-            ret[l - k[1] + 1, l - j[1] + 3] =  -.5
+        for l=1:m-2
+            ret[l, l + 2] =  -.5
         end
         
         ret    
     else
-        for l=max(k[1],j[1]):min(k[end],j[end])
-            ret[l - k[1] + 1, l - j[1] + 1] =  λ./(l - k[1] + λ)
+        for l=1:m
+            ret[l, l] =  λ./(l - 1 + λ)
         end
         
-        for l=max(k[1],j[1]):min(k[end],j[end])-2
-            ret[l - k[1] + 1, l - j[1] + 3] =  -λ./(l - k[1] + λ + 2)
+        for l=1:m-2
+            ret[l, l + 2] =  -λ./(l + λ + 1)
         end
         
         ret    
     end
 end
 
-function conversionmatrix(od::Range1,k::Range1,j::Range1)
-    @assert k[1] == 1 && j[1] == 1
-    
-    m = max(k[end],j[end])
-    
+function conversionmatrix(od::Range1,m::Integer)        
     ret = speye(m,m)
     
     for λ = od[1]:od[end]-1
-       ret = conversionmatrix(λ,1:m,1:m)*ret 
+       ret = conversionmatrix(λ,m)*ret 
     end
     
-    ret[k,j]
+    ret
 end
 
-function derivativematrix(μ::Integer,od::Range1,d::Domain,k::Range1,j::Range1)
+function derivativematrix(μ::Integer,od::Range1,d::Domain,m::Integer)
     @assert od[1] == 0  #TODO: more general
-    @assert k[1] == 1 && j[1] == 1
     
-    conversionmatrix(μ:od[end],k,j)*derivativematrix(μ,d,k,j)
+    conversionmatrix(μ:od[end],m)*derivativematrix(μ,d,m)
 end
 
 
 function Base.getindex(op::DifferentialOperator,k::Range1,j::Range1)
-    @assert k[1] == 1 && j[1] == 1
+    m = max(k[end],j[end])
     
-    ret = spzeros(k[end],j[end])
+    ret = spzeros(m,m)
     
     od = differentialorder(op)
     
     for l = 1:length(op.coefficients)
-        ret += op.coefficients[l]*derivativematrix(l-1,0:od,op.domain,k,j)
+        ret += op.coefficients[l]*derivativematrix(l-1,0:od,op.domain,m)
     end
     
-    ret
+    ret[k,j]
 end
 
 
@@ -140,7 +130,7 @@ Base.getindex(op::DifferentialOperator,k::Integer,j::Integer)=op[k:k,j:j][1,1]
 
 ## ultraconv
 
-ultraconv(v::Vector{Float64},μ::Integer)=conversionmatrix(0:μ,1:length(v),1:length(v))*v
+ultraconv(v::Vector{Float64},μ::Integer)=conversionmatrix(0:μ,length(v))*v
 coefficients(f::IFun,m::Integer)=ultraconv(f.coefficients,m)
 
 
@@ -155,6 +145,8 @@ function tomatrix(A::Array{Operator,1},n::Integer)
 end
 
 function \(A::Array{Operator,1},b::Array{Any,1})
+    @assert length(A) == length(b)
+
     d=A[1].domain
     
     map(x -> (@assert d == x.domain),A)
@@ -164,18 +156,30 @@ function \(A::Array{Operator,1},b::Array{Any,1})
     
     m = mapreduce(x-> typeof(x) <: IFun ? length(x) : 1, +, b)
     
-    for logn = 1:20
+    rhs=copy(b)
+    
+    for logn = 3:20
         n = 2^logn + m
     
         M = tomatrix(A,n)
+        
+        #number of bc rows
         nbc=mapreduce(a-> size(a)[1] == Inf ? 0 : size(a)[1],+,A)
+        #size of each part
+        sz = map(a-> size(a)[1]== Inf ? n - nbc : size(a)[1],A)
         
+
         
+        for j = 1:length(b)
+            if typeof(b[j]) == IFun
+                rhs[j] = pad(coefficients(x,differentialorder(A)),n-nbc) 
+            else
+                rhs[j] = b[j].*ones(sz[j])
+            end
+        end
+
         
-        
-        rhs=mapreduce(x->typeof(x) <: IFun ? pad(coefficients(x,differentialorder(A)),n-nbc) : [x], vcat, b)
-        
-        cfs = M\rhs
+        cfs = M\vcat(rhs...)
         
         if (maximum(abs(cfs[end-8:end])) < eps())
             return IFun(chop(cfs,eps()),d)
