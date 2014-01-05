3ee0cf1cb9ed2ed20b8f7c190b94396458ce281e
Sheehan Olver <solver@mac.com>
2014-01-05 21:13:18 +1100

    Sped up givens

--- src/Operators/BandedOperator.jl
+++ src/Operators/BandedOperator.jl
@@ -38,71 +38,74 @@ datalength(b::MutableBandedOperator)=b.datalength
 
 
 function Base.getindex(b::MutableBandedOperator,kr::Range1,jr::Range1)
-  ret = spzeros(length(kr),length(jr))
-  
-  for k = kr
-      ir = bandrange(b) + k
+    ret = spzeros(length(kr),length(jr))
     
-    for j=max(ir[1],jr[1]):min(ir[end],jr[end])
-  
-      if k > datalength(b)
-        ret[k-kr[1]+1,j-jr[1]+1] = b.op[k,j]
-      else
-        ret[k-kr[1]+1,j-jr[1]+1] = b.data[k,j - k]
-      end
+    for k = kr
+        ir = bandrange(b) + k
+        
+        for j=max(ir[1],jr[1]):min(ir[end],jr[end])
+            ret[k-kr[1]+1,j-jr[1]+1] = b[k,j]
+        end
     end
-  end
-  
-  ret
+    
+    ret
+end
+
+function Base.getindex(b::MutableBandedOperator,k::Integer,j::Integer)  
+    ir = bandrange(b) + k
+    
+    if k > datalength(b)
+        b.op[k,j]
+    else
+        b.data[k,j - k]
+    end
 end
 
 getindex!(b::MutableBandedOperator,kr::Range1,jr::Range1)=resizedata!(b,kr[end])[kr,jr]
 getindex!(b::MutableBandedOperator,kr::Integer,jr::Integer)=resizedata!(b,kr)[kr,jr]
 
 function resizedata!{T<:Number,M<:BandedOperator}(b::MutableBandedOperator{T,M},n::Integer)
-  l = datalength(b)
-  if n > l
-    resize!(b.data,2n,length(bandrange(b)))
-    
-    for k=l+1:n
-      for j=indexrange(b,k)
-        b.data[k,j-k] = b.op[k,j]
-      end
-      
-      b.datalength = n;
+    l = datalength(b)
+    if n > l
+        resize!(b.data,2n,length(bandrange(b)))
+        
+        copybandedentries(b.op,b.data,l+1:n,bandrange(b))
+        
+        b.datalength = n
     end
-  end
-  
-  b
+    
+    b
 end
 
 function Base.setindex!(b::MutableBandedOperator,x,k::Integer,j::Integer)
-  resizedata!(b,k)
+    resizedata!(b,k)
   
-  if abs(b[k,j] - x) > 10eps()
-      sh = bandrange(b)[1] 
-      b.data[k,j-k] = x
-      x
-  end
+    sh = bandrange(b)[1] 
+    b.data[k,j-k] = x
+    x
 end
 
 function givensreduce!(B::MutableBandedOperator,k1::Integer,k2::Integer,j1::Integer)
-  a=getindex!(B,k1,j1);b=getindex!(B,k2,j1);
-  sq=sqrt(a*a + b*b);
-  a=a/sq;b=b/sq;
-  
-  #TODO: Assuming that left rows are already zero
-  
-  for j = j1:indexrange(B,k1)[end]
-    na = a*B[k1,j] + b*B[k2,j]
-    nb = -b*B[k1,j] + a*B[k2,j]
+    a=getindex!(B,k1,j1);b=getindex!(B,k2,j1);
+    sq=sqrt(a*a + b*b);
+    a=a/sq;b=b/sq;
     
-    B[k1,j] = na
-    B[k2,j] = nb
-  end
-  
-  #TODO: assert that the remaining of k2 are zero
-  B
+    #TODO: Assuming that left rows are already zero
+    
+    for j = j1:indexrange(B,k1)[end]
+        B1 = B[k1,j]
+        B2 = B[k2,j]
+        
+        B[k1,j] = a*B1 + b*B2
+        B[k2,j] = -b*B1 + a*B2
+    end
+    
+    for j=indexrange(B,k1)[end]+1:indexrange(B,k2)[end]
+        B[k2,j] = a*B[k2,j]
+    end
+    
+    #TODO: assert that the remaining of k2 are zero
+    B
 end
 
 function givensreduce!(B::MutableBandedOperator,k1::Range1,j1::Integer)
