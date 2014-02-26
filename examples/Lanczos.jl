c5345de5f5baf69ad982d6cb894593266a774580
Sheehan Olver <solver@mac.com>
2014-02-26 12:20:05 +1100

    Added Lanczos.jl

---
+++ examples/Lanczos.jl
@@ -0,0 +1,59 @@
+function lanczos(w,N)
+    x = Fun(identity,domain(w))
+    P = Array(IFun,N + 1)
+    β = Array(Float64,N)
+    γ = Array(Float64,N)
+
+    P[1] = Fun([1./sqrt(sum(w))],x.domain)
+
+    v = x.*P[1]
+    β[1] = sum(w.*v.*P[1])
+
+    v = v - β[1]*P[1]
+    γ[1] = sqrt(sum(w.*v.^2))
+
+    P[2] = v/γ[1]
+
+    for k = 2:N
+        v = x.*P[k] - γ[k-1]*P[k-1]
+        β[k] = sum(w.*v.*P[k])
+        v = v - β[k]*P[k]
+        γ[k] = sqrt(sum(w.*v.^2))
+        P[k+1] = v/γ[k]
+    end
+    
+    β,γ
+end
+
+
+
+ # Semicircle law
+x=Fun(x->x,[-2.,2.])
+w=sqrt(4-x.^2)/(2π)
+
+lanczos(w,5)
+
+
+# Marchenco–Pastur law
+
+r = .5
+lmax = (1+sqrt(r))^2
+lmin = (1-sqrt(r))^2
+
+x= Fun(identity,[lmin,lmax])
+
+f=sqrt((lmax-x).*(x-lmin))./x
+
+
+# Wachter law
+
+a = 5; b= 10;
+c = sqrt(a/(a+b)*(1-1/(a+b)))
+d = sqrt(1/(a+b)*(1-a/(a+b)))
+
+lmax = (c+d)^2
+lmin = (c-d)^2
+
+x = Fun(identity,[lmin,lmax])
+
+w = (a+b) * sqrt((x-lmin).*(lmax-x))./(2π*x.*(1-x))
