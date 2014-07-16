7b35239530a80ec349bc458ad2acd12fa71e73ba
Sheehan Olver <solver@mac.com>
2014-07-16 15:15:14 +1000

    General rays

--- src/Domains/Ray.jl
+++ src/Domains/Ray.jl
@@ -10,8 +10,6 @@ export Ray
 type Ray <: IntervalDomain
     centre::Float64  ##TODO Allow complex
     angle::Float64
-    
-    Ray(c,a)=(@assert a==0.; new(c,a))
 end
 
 Ray()=Ray(0.,0.)
@@ -25,10 +23,20 @@ ray_tocanonicalD(x)=2./((1.+x).^2)
 ray_fromcanonical(x)=(1.+x)./(1.-x)
 ray_fromcanonicalD(x)=2./((x.-1.).^2)
 
-tocanonical(d::Ray,x)=ray_tocanonical(x.-d.centre)
-tocanonicalD(d::Ray,x)=ray_tocanonicalD(x.-d.centre)
-fromcanonical(d::Ray,x)=ray_fromcanonical(x).+d.centre
-fromcanonicalD(d::Ray,x)=ray_fromcanonicalD(x)
+function cistyped(a::Real)
+    if a==0
+        1.
+    elseif a==π
+        -1.
+    else
+        cis(a)
+    end
+end
+
+tocanonical(d::Ray,x)=ray_tocanonical(cistyped(-d.angle).*(x.-d.centre))
+tocanonicalD(d::Ray,x)=cistyped(-d.angle).*ray_tocanonicalD(cistyped(-d.angle).*(x.-d.centre))
+fromcanonical(d::Ray,x)=cistyped(d.angle)*ray_fromcanonical(x).+d.centre
+fromcanonicalD(d::Ray,x)=cistyped(d.angle)*ray_fromcanonicalD(x)
 
 
 
