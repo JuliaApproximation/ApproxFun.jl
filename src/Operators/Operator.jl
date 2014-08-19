079d95d9c4c8eb7afb48d493f2369328ae20df41
Sheehan Olver <solver@mac.com>
2014-08-19 21:23:45 +1000

    added periodic for periodic bcs

--- src/Operators/Operator.jl
+++ src/Operators/Operator.jl
@@ -1,5 +1,5 @@
 export Operator,Functional,InfiniteOperator
-export bandrange, linsolve
+export bandrange, linsolve, periodic
 
 
 
@@ -164,6 +164,23 @@ function dirichlet{T<:IntervalDomain}(d::Vector{T})
     B=zeros(Operator,2m,m)
     B[1,1]=dirichlet(d[1])[1]
     B[2,end]=dirichlet(d[end])[end]
+    for k=1:m-1
+        B[k+2,k]=dirichlet(d[k])[2]
+        B[k+2,k+1]=-dirichlet(d[k+1])[1]    
+        B[k+m+1,k]=neumann(d[k])[2]
+        B[k+m+1,k+1]=-neumann(d[k+1])[1]        
+    end
+    B
+end
+
+function periodic{T<:IntervalDomain}(d::Vector{T})
+    m=length(d)
+    B=zeros(Operator,2m,m)
+    B[1,1]=dirichlet(d[1])[1]
+    B[1,end]=-dirichlet(d[end])[end]
+    B[2,1]=neumann(d[1])[1]
+    B[2,end]=-neumann(d[end])[end]
+
     for k=1:m-1
         B[k+2,k]=dirichlet(d[k])[2]
         B[k+2,k+1]=-dirichlet(d[k+1])[1]    
