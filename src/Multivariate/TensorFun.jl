033fb8578047d7251a68240af21b0543965e6e09
Sheehan Olver <solver@mac.com>
2014-09-01 14:32:25 +1000

    coefficients(TensorFun,n,m)

--- src/Multivariate/TensorFun.jl
+++ src/Multivariate/TensorFun.jl
@@ -3,37 +3,52 @@ export TensorFun
 
 
 type TensorFun{F<:IFun,D<:IntervalDomain}
-    coefficients::Vector{F}##coefficients are in x
+    coefficients::Vector{F}     # coefficients are in x
     domainy::D
 end
 
-function TensorFun(f::Fun2D)
-    cfs=coefficients(f)
-    dx=domain(f,1)
-    dy=domain(f,2)
-    ret=Array(typeof(f.A[1]),size(cfs,2))
+function TensorFun{T<:Number}(cfs::Array{T},dx,dy)
+    ret=Array(IFun{T,typeof(dx)},size(cfs,2))
     for k=1:size(cfs,2)
-        ret[k]=IFun(cfs[:,k],dx)
+        ret[k]=IFun(chop!(cfs[:,k],10eps()),dx)
     end
     TensorFun(ret,dy)
 end
 
+TensorFun(f::Fun2D)=TensorFun(coefficients(f),domain(f,1),domain(f,2))
 
 TensorFun(f::Function,d1...)=TensorFun(Fun2D(f,d1...))
 
 Base.size(f::TensorFun,k::Integer)=k==1?mapreduce(length,max,f.coefficients):length(f.coefficients)
 Base.size(f::TensorFun)=(size(f,1),size(f,2))
 
-function coefficients{T<:Number}(f::TensorFun{IFun{T}})
-    A=zeros(T,size(f)...)
-    for k=1:length(f.coefficients)
-        A[1:length(f.coefficients[k]),k]=f.coefficients[k].coefficients
+
+function funlist2coefficients{T<:Number,D<:IntervalDomain}(f::Vector{IFun{T,D}})
+    A=zeros(T,mapreduce(length,max,f),length(f))
+    for k=1:length(f)
+        A[1:length(f[k]),k]=f[k].coefficients
     end
-end 
+end
+
+coefficients(f::TensorFun)=funlist2coefficients(f.coefficients)
+
+function coefficients(f::TensorFun,ox::Integer,oy::Integer)
+    m=size(f,1)
+    A=[pad!(coefficients(fx,ox),m) for fx in f.coefficients]
+    B=hcat(A...)::Array{Float64,2}
+    for k=1:size(B,1)
+        B[k,:]=ultraconversion(vec(B[k,:]),oy)
+    end
+    
+    B
+end
+
 
 domain(f::TensorFun,k::Integer)=k==1?domain(f.coefficients[1]):f.domainy
 
 
 
 
-Fun2D(f::TensorFun)=Fun2D(f.coefficients,domain(f,2))
+Fun2D(f::TensorFun)=Fun2D(f.coefficients.',domain(f,2))
+
+.'(f::TensorFun)=TensorFun(coefficients(f).',domain(f,2),domain(f,1))
