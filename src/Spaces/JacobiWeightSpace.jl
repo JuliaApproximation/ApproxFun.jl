da26fe57786ad05bca3cef5fc90d0a684f0adcb6
Sheehan Olver <solver@mac.com>
2014-09-10 10:40:40 +1000

    Adding evaluate

--- src/Spaces/JacobiWeightSpace.jl
+++ src/Spaces/JacobiWeightSpace.jl
@@ -9,3 +9,6 @@ immutable JacobiWeightSpace{α,β} <: IntervalDomainSpace
 end
 
 jacobiweight(α,β,x)=(1.+x).^α.*(1.-x).^β
+jacobiweight{α,β}(sp::JacobiSpace{α,β})=jacobiweight(α,β,tocanonical(sp,x))
+
+evaluate(sp::JacobiWeightSpace,cfs::Vector,x)=evaluate(ChebyshevSpace(domain(sp)),cfs,x).*jacobiweight(sp,x)
