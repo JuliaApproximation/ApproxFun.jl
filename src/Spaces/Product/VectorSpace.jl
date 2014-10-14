e3aa239b4bd00ecfe6980abd050f0528d90300cc
Sheehan Olver <solver@mac.com>
2014-10-14 15:13:21 +1100

    Added vector space

--- src/Spaces/Product/VectorSpace.jl
+++ src/Spaces/Product/VectorSpace.jl
@@ -1,8 +1,34 @@
 ## VectorSpace{T,S} encodes a space that is a Vector, with coefficients interlaced
 
 
-# immutable VectorSpace{S} <: FunctionSpace
-#     space::S
-#     length::Int
-# end
+immutable VectorDomainSpace{S,T} <: DomainSpace{T}
+     space::S
+     length::Int
+ end
+
+VectorDomainSpace{T}(S::DomainSpace{T},n)=VectorDomainSpace{typeof(S),T}(S,n)
+
+domain(S::VectorDomainSpace)=domain(S.space)
+transform(S::VectorDomainSpace,vals::Vector)=transform!(S,hcat(vals...).')
+
+
+function transform!(S::VectorDomainSpace,M::Array)
+    @assert size(M,2)==S.length
+    for k=1:size(M,2)
+        M[:,k]=transform(S.space,M[:,k])
+    end
+    vec(M.')
+end
+
+Base.vec{S<:DomainSpace,V,T}(f::Fun{VectorDomainSpace{S,V},T})=Fun{S,T}[Fun(f.coefficients[j:f.space.length:end],f.space.space) for j=1:f.space.length]
+function devec{S,T}(v::Vector{Fun{S,T}})
+    @assert mapreduce(space,isequal,v)
+    Fun(vec(coefficients(v).'),VectorDomainSpace(space(first(v)),length(v)))
+end
+
+evaluate{V<:VectorDomainSpace,T}(f::Fun{V,T},x)=evaluate(vec(f),x)
+
+for op in (:differentiate,:integrate,:(Base.cumsum))
+    @eval $op{V<:VectorDomainSpace}(f::Fun{V})=devec(map($op,vec(f)))
+end
 
