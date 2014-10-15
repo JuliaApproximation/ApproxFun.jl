15a7ffaefa69f88ff8b54329057ec2ec94b0b441
Sheehan Olver <solver@mac.com>
2014-10-15 14:20:45 +1100

    Simplify linsolve

--- src/Operators/linsolve.jl
+++ src/Operators/linsolve.jl
@@ -60,6 +60,7 @@ Fun_coefficients(b::Vector,sp)=vcat(map(f-> isa(f,Fun)? coefficients(f,sp) :  f,
 linsolve{T<:Operator,N<:Number}(A::Vector{T},b::Array{N};tolerance=0.01eps(),maxlength=1000000)=adaptiveqr(promotedomainspace(A),b,tolerance,maxlength)
 
 function linsolve{T<:Operator}(A::Vector{T},b::Array;tolerance=0.01eps(),maxlength=1000000)
+    @assert size(b,2)==1  #TODO: reimplemnt
     A=promotedomainspace(A)
     
     for k=1:length(A)-1
@@ -86,60 +87,10 @@ function linsolve{T<:Operator}(A::Vector{T},b::Array;tolerance=0.01eps(),maxleng
     Fun(u,commondomainspace(A,b))
 end
 
-
- function linsolve{T<:Operator,M<:Number}(A::Array{T,2},b::Vector{M};tolerance=0.01eps(),maxlength=1000000)
-    m = size(A,2)
- 
-     ret=adaptiveqr(interlace(A),b,tolerance,maxlength)  #Given just an array, we don't know how to interlace
-                                                         #so assume user knows, this is correct for bc rows
-                                     
-                                     
-     Fun[Fun(ret[k:m:end],commondomain(A[:,k])) for k=1:m]
- end
- 
- 
-function linsolve{T<:Operator,M<:Number}(A::Array{T,2},b::Array{M,2};tolerance=0.01eps(),maxlength=1000000)
-    m = size(A,2)
-    
-    ret=adaptiveqr(interlace(A),b,tolerance,maxlength)  #Given just an array, we don't know how to interlace
-                                                     #so assume user knows, this is correct for bc rows
-                                 
-                                 
-    [Fun(ret[k:m:end,j],commondomain(A[:,k])) for k=1:m,j=1:size(b,2)]
-end
  
 
-scalarorfuntype{S,T<:Number}(::Fun{S,T})=T
-scalarorfuntype{T<:Number}(::T)=T
-scalarorfuntype{T<:Number}(b::Vector{T})=T
-scalarorfuntype(b::Vector{Any})=promote_type(map(scalarorfuntype,b)...)
-scalarorfuntype{F<:Fun}(b::Vector{F})=promote_type(map(scalarorfuntype,b)...)
  
-function linsolve{T<:Operator}(A::Array{T,2},b::Vector{Any};kwds...)
-    m,n=size(A)
-
-    br=m-n
-
-    l=mapreduce(length,max,b[br+1:end])
-
-    r=zeros(scalarorfuntype(b),br+n*l)
-    
-    r[1:br]=b[1:br]
-    
-    for k=br+1:m
-        sp=findmaxrangespace([A[k,:]...])
-        if k > length(b)## assume its zer
-            r[k:n:end]=zeros(l)
-        elseif isa(b[k],Fun)
-            ##TODO: boiunds check
-            r[k:n:end]=pad(coefficients(b[k],sp),l)
-        else  #type is scalar
-            r[k:n:end]=pad([b[k]],l)
-        end
-    end
-
-    linsolve(A,r;kwds...)
-end 
+linsolve{T<:Operator}(A::Array{T,2},b;kwds...)=linsolve(interlace(A),b;kwds...)
 
 
 linsolve(A::Operator,b::Array;kwds...)=linsolve([A],b;kwds...)
