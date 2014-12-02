b1a92edd1ea6579e68e61e87336c09870b532528
Sheehan Olver <solver@mac.com>
2014-12-02 17:40:33 -0300

    Fixed #51: Fun([]) is now interpreted as zero

--- src/LinearAlgebra/clenshaw.jl
+++ src/LinearAlgebra/clenshaw.jl
@@ -17,21 +17,25 @@ function clenshaw(c,x)
 end
 
 function clenshaw(c::Vector,x::Real)
-
+    if isempty(c)
+        return zero(x)
+    end
+    
     x = 2x;
-    bk1 = 0.0;
-    bk2 = 0.0;
+    bk1 = 0.0;bk2 = 0.0
     for k = length(c):-1:2
-        bk2, bk1 = bk1, c[k] + x * bk1 - bk2
+        bk2, bk1 = bk1, c[k] + x*bk1 - bk2
     end
 
-    c[1] + 0.5 * x * bk1 - bk2
+    c[1] + 0.5x*bk1 - bk2
 end
 
 
 
 function clenshaw{T<:Number,M<:Real}(c::Vector{T},x::Vector{M})
-    @assert length(c) > 0 
+    if isempty(c)
+        return zeros(x)
+    end
     
     n = length(x)
     clenshaw(c,x,ClenshawPlan(T,n))
@@ -47,9 +51,7 @@ function clenshaw{T<:Number}(c::Array{T,2},x::Vector{T},plan::ClenshawPlan{T})
     bk2=plan.bk2
 
 
-    n=size(c)[2] #number of funs
-    m=size(c)[1] #number of coefficients
-
+    m,n=size(c) # m is # of coefficients, n is # of funs
     
     for i = 1:n
         @inbounds bk1[i] = 0.
@@ -61,7 +63,7 @@ function clenshaw{T<:Number}(c::Array{T,2},x::Vector{T},plan::ClenshawPlan{T})
         for j=1:n
             ck = c[k,j]
 
-            @inbounds bk[j] = ck + 2x[j] * bk1[j] - bk2[j]
+            @inbounds bk[j] = ck + 2x[j]*bk1[j] - bk2[j]
         end
         
         bk2, bk1, bk = bk1, bk, bk2
@@ -70,7 +72,7 @@ function clenshaw{T<:Number}(c::Array{T,2},x::Vector{T},plan::ClenshawPlan{T})
 
     for j = 1:n
         ce = c[1,j]
-        @inbounds bk[j] = ce + x[j] * bk1[j] - bk2[j]
+        @inbounds bk[j] = ce + x[j]*bk1[j] - bk2[j]
     end
     
     bk    
@@ -119,6 +121,10 @@ end
 #Clenshaw routine for many points
 #Note that bk1, bk2, and bk are overwritten
 function clenshaw{T<:Number,M<:Real}(c::Vector{T},x::Vector{M},plan::ClenshawPlan{T})
+    if isempty(c)
+        return(zeros(x))
+    end
+
     bk=plan.bk    
     bk1=plan.bk1
     bk2=plan.bk2
@@ -135,15 +141,15 @@ function clenshaw{T<:Number,M<:Real}(c::Vector{T},x::Vector{M},plan::ClenshawPla
 
     for k in  length(c):-1:2
         ck = c[k]
-        for i in 1 : n
+        for i in 1:n
             @inbounds bk[i] = ck + 2x[i] * bk1[i] - bk2[i]
         end
         bk2, bk1, bk = bk1, bk, bk2
     end
 
     ce = c[1]
-    for i in 1 : n
-        @inbounds  bk[i] = ce + x[i] * bk1[i] - bk2[i]
+    for i in 1:n
+        @inbounds  bk[i] = ce + x[i]*bk1[i] - bk2[i]
     end
     
     bk
@@ -153,11 +159,19 @@ end
 # overwrite x
 clenshaw!{T<:Real}(c::Vector{T},x::Vector{T})=clenshaw!(c,x,ClenshawPlan(T,length(x)))
 function clenshaw!{T<:Real}(c::Vector{T},x::Vector{T},plan::ClenshawPlan{T})
+    n = length(x)
+    
+    if isempty(c)
+        for k=1:n
+            x[k]=zero(T)
+        end
+        return x
+    end
+
     bk=plan.bk    
     bk1=plan.bk1
     bk2=plan.bk2
 
-    n = length(x)
 #    x=2x
 
     
