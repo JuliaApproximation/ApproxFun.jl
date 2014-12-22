76ca77b1fe3253b34b8885d5e67157aa1436f2f6
Richard Mikael Slevinsky <Richard.Slevinsky@maths.ox.ac.uk>
2014-12-22 11:11:44 +0000

    New extra function: digits
    Used to get and set bits precision in terms of base-10 digits.

--- src/Extras/Extras.jl
+++ src/Extras/Extras.jl
@@ -3,3 +3,12 @@ include("roots.jl")
 include("sample.jl")
 include("timeevolution.jl")
 include("fftBigFloat.jl")
+
+export digits
+
+#
+# This function provides a convenient way to query or specify the BigFloat precision.
+# It should be moved to a better file location when BigFloat support is settled.
+#
+digits(n::Integer) = set_bigfloat_precision(int(ceil(n*log2(10))))
+digits() = int(floor(get_bigfloat_precision()*log10(2)))
