c8c563ca2eb1f4a34bd9e4f04635dd6d79ff2d54
Richard Mikael Slevinsky <Richard.Slevinsky@maths.ox.ac.uk>
2014-12-30 17:14:54 -0700

    This adds a fallback general right multiplication
    For some reason LightTable seems to update/remove all the spaces. I’ll
    check if i can get it to ignore this.

--- src/Operators/Multiplication.jl
+++ src/Operators/Multiplication.jl
@@ -3,7 +3,7 @@ abstract AbstractMultiplication{T} <:BandedOperator{T}
 immutable Multiplication{D<:FunctionSpace,S<:FunctionSpace,T<:Number} <: AbstractMultiplication{T}
     f::Fun{D,T}
     space::S
-    
+
     Multiplication(f::Fun{D,T},sp::S)=new(f,sp)
 end
 
@@ -13,7 +13,8 @@ Multiplication(f::Fun)=Multiplication(f,AnySpace())
 
 Multiplication(c::Number)=ConstantOperator(c)
 
-
+# This covers right multiplication unless otherwise specified.
+Multiplication{D,T}(S::FunctionSpace,f::Fun{D,T}) = Multiplication(f,S)
 
 
 domainspace{D,S,T}(M::Multiplication{D,S,T})=M.space
@@ -27,7 +28,7 @@ bandinds{F,T}(D::Multiplication{F,AnySpace,T})=error("No range space attached to
 addentries!{F,T}(D::Multiplication{F,AnySpace,T},A,kr)=error("No range space attached to Multiplication")
 
 
-function addentries!{F,S,T}(D::Multiplication{F,S,T},A,kr)   
+function addentries!{F,S,T}(D::Multiplication{F,S,T},A,kr)
     # Default is to convert to space of f
     sp=domainspace(D)
     csp=space(D.f)
@@ -36,23 +37,23 @@ function addentries!{F,S,T}(D::Multiplication{F,S,T},A,kr)
     end
     addentries!(TimesOperator([Multiplication(D.f,csp),Conversion(sp,csp)]),A,kr)
 end
-        
+
 function bandinds{F,S,T}(D::Multiplication{F,S,T})
     sp=domainspace(D)
     csp=space(D.f)
     if csp==sp
         error("Override bandinds for Multiplication(::Fun{"*string(typeof(space(D.f)))*",T},"*string(typeof(sp))*")")
-    end     
+    end
     bandinds(TimesOperator([Multiplication(D.f,csp),Conversion(sp,csp)]))
 end
 
-# corresponds to default implementation        
+# corresponds to default implementation
 function rangespace{F,S,T}(D::Multiplication{F,S,T})
     sp=domainspace(D)
     csp=space(D.f)
     if csp==sp
         error("Override rangespace for Multiplication(::Fun{"*string(typeof(space(D.f)))*",T},"*string(typeof(sp))*")")
-    end      
+    end
     rangespace(TimesOperator([Multiplication(D.f,csp),Conversion(sp,csp)]))
 end
 
@@ -67,7 +68,6 @@ promotedomainspace(D::AbstractMultiplication,sp::FunctionSpace)=Multiplication(D
 
 
 Base.diagm(a::Fun)=Multiplication(a)
-
 
 immutable MultiplicationWrapper{D<:FunctionSpace,O<:BandedOperator,T<:Number} <: AbstractMultiplication{T}
     f::Fun{D,T}
@@ -85,12 +85,12 @@ end
 
 
 
-## Multiplication operators allowus to multiply two spaces
+## Multiplication operators allow us to multiply two spaces
 
 # Overrideable
 # This should be overriden whenever the multiplication space is different
 function .*{T,N,S,V}(f::Fun{S,T},g::Fun{V,N})
-    # When the spaces differ we promote and multiply   
+    # When the spaces differ we promote and multiply
     if domainscompatible(space(f),space(g))
         # THe bandwidth of Mutliplication is
         # usually the length of the function
@@ -98,8 +98,8 @@ function .*{T,N,S,V}(f::Fun{S,T},g::Fun{V,N})
             Multiplication(f,space(g))*g
         else
             Multiplication(g,space(f))*f
-        end        
-    else         
+        end
+    else
         sp=union(space(f),space(g))
         Fun(f,sp).*Fun(g,sp)
     end
@@ -109,8 +109,8 @@ end
 function transformtimes(f::Fun,g::Fun,n)
     @assert spacescompatible(space(f),space(g))
     f2 = pad(f,n); g2 = pad(g,n)
-    
+
     sp=space(f)
-    chop!(Fun(transform(sp,values(f2).*values(g2)),sp),10eps())    
+    chop!(Fun(transform(sp,values(f2).*values(g2)),sp),10eps())
 end
 transformtimes(f::Fun,g::Fun)=transformtimes(f,g,length(f) + length(g) - 1)
--- src/Spaces/Ultraspherical/UltrasphericalOperators.jl
+++ src/Spaces/Ultraspherical/UltrasphericalOperators.jl
@@ -21,11 +21,11 @@ function evaluatechebyshev{T<:Number}(n::Integer,x::T)
         p = zeros(T,n)
         p[1] = one(T)
         p[2] = x
-        
+
         for j=2:n-1
             p[j+1] = 2x*p[j] - p[j-1]
         end
-        
+
         p
     end
 end
@@ -55,7 +55,7 @@ function Base.getindex(op::Evaluation{Chebyshev,Bool},k::Range)
 end
 
 function Base.getindex(op::Evaluation{Chebyshev},k::Range)
-    if op.order == 0    
+    if op.order == 0
         evaluatechebyshev(k[end],tocanonical(domain(op),op.x))[k]
     else
         error("Only zero–second order implemented")
@@ -73,18 +73,18 @@ Base.stride{U<:Ultraspherical,V<:Ultraspherical}(M::Multiplication{U,V})=stride(
 
 function chebmult_addentries!(cfs::Vector,A,kr::Range)
     toeplitz_addentries!(.5cfs,A,kr)
-    hankel_addentries!(.5cfs,A,intersect(2:kr[end],kr))            
+    hankel_addentries!(.5cfs,A,intersect(2:kr[end],kr))
 end
 
 
 for TYP in (:Range,:UnitRange) # needed to avoid confusion
     @eval begin
         addentries!{D<:Ultraspherical}(M::Multiplication{D,Chebyshev},A,kr::$TYP)=chebmult_addentries!(canonicalcoefficients(M.f),A,kr)
-    
+
         function addentries!{D<:Ultraspherical}(M::Multiplication{D,Ultraspherical{1}},A,kr::$TYP)
             cfs=canonicalcoefficients(M.f)
             toeplitz_addentries!(.5cfs,A,kr)
-            hankel_addentries!(-.5cfs[3:end],A,kr)    
+            hankel_addentries!(-.5cfs[3:end],A,kr)
         end
     end
 end
@@ -94,7 +94,7 @@ end
 function addentries!{D<:Ultraspherical,λ}(M::Multiplication{D,Ultraspherical{λ}},A,kr::UnitRange)
     a=coefficients(M.f,domainspace(M))
     for k=kr
-        A[k,k]=a[1] 
+        A[k,k]=a[1]
     end
 
     if length(a) > 1
@@ -104,12 +104,12 @@ function addentries!{D<:Ultraspherical,λ}(M::Multiplication{D,Ultraspherical{λ
         C1=2λ*J
         addentries!(C1,a[2],A,kr)
         C0=isbaeye(jkr)
-        for k=1:length(a)-2    
+        for k=1:length(a)-2
             C1,C0=2(k+λ)/(k+1)*J*C1-(k+2λ-1)/(k+1)*C0,C1
-            addentries!(C1,a[k+2],A,kr)    
+            addentries!(C1,a[k+2],A,kr)
         end
     end
-    
+
     A
 end
 
@@ -124,7 +124,7 @@ end
 
 rangespace{λ}(D::Derivative{Ultraspherical{λ}})=Ultraspherical{λ+D.order}(domain(D))
 bandinds{S<:Ultraspherical}(D::Derivative{S})=0,D.order
-bandinds{S<:Ultraspherical}(D::Integral{S})=-D.order,0   
+bandinds{S<:Ultraspherical}(D::Integral{S})=-D.order,0
 Base.stride{S<:Ultraspherical}(D::Derivative{S})=D.order
 
 function addentries!{λ}(D::Derivative{Ultraspherical{λ}},A,kr::Range)
@@ -140,14 +140,14 @@ function addentries!{λ}(D::Derivative{Ultraspherical{λ}},A,kr::Range)
         end
     else
         C=pochhammer(1.λ,m)*(4./(d.b-d.a)).^m
-        for k=kr        
+        for k=kr
             A[k,k+m] += C
         end
     end
-    
+
     A
 end
- 
+
 
 ## Integral
 
@@ -174,7 +174,7 @@ function addentries!{λ}(D::Integral{Ultraspherical{λ}},A,kr::Range)
             A[k,k-m] += C
         end
     end
-    
+
     A
 end
 
@@ -194,16 +194,16 @@ function Conversion{a,b}(A::Ultraspherical{a},B::Ultraspherical{b})
         d=domain(A)
         Conversion(Ultraspherical{b-1}(d),B)*Conversion(A,Ultraspherical{b-1}(d))
     end
-end   
+end
 
 
 function addentries!(M::Conversion{Chebyshev,Ultraspherical{1}},A,kr::Range)
     for k=kr
         A[k,k] += (k == 1)? 1. : .5
-        A[k,k+2] += -.5        
+        A[k,k+2] += -.5
     end
-    
-    A    
+
+    A
 end
 
 function addentries!{m,λ}(M::Conversion{Ultraspherical{m},Ultraspherical{λ}},A,kr::Range)
@@ -212,39 +212,39 @@ function addentries!{m,λ}(M::Conversion{Ultraspherical{m},Ultraspherical{λ}},A
         A[k,k] += (λ-1.)./(k - 2. + λ)
         A[k,k+2] += -(λ-1.)./(k + λ)
     end
-    
-    A    
+
+    A
 end
 
 function multiplyentries!(M::Conversion{Chebyshev,Ultraspherical{1}},A,kr::Range)
     cr=columnrange(A)::Range1{Int}
-    
+
     #We assume here that the extra rows are redundant
     for k=max(2,kr[1]):kr[end]+2,j=cr
         A[k,j] *= .5
     end
-    
+
     #We assume that A has allocated 2 more bandwidth
     for k=max(1,kr[1]):kr[end],j=(cr[1]+2):cr[end]
         A[k,j] -= A[k+2,j-2]
-    end 
+    end
 end
 
 function multiplyentries!{m,λ}(M::Conversion{Ultraspherical{m},Ultraspherical{λ}},A,kr::Range)
     @assert λ==m+1
     cr=columnrange(A)::Range1{Int64}
-    
+
     λf = 1.λ
-    
+
     #We assume here that the extra rows are redundant
     for k=max(kr[1],1):kr[end]+2,j=cr
         A[k,j] *= (λf-1)./(k - 2. + λf)
     end
-    
+
     #We assume that A has allocated 2 more bandwidth
     for k=max(kr[1],1):kr[end],j=(cr[1]+2):cr[end]
         A[k,j] -= A[k+2,j-2]
-    end 
+    end
 end
 
 bandinds{m,λ}(C::Conversion{Ultraspherical{m},Ultraspherical{λ}})=0,2
@@ -256,7 +256,7 @@ Base.stride{m,λ}(C::Conversion{Ultraspherical{m},Ultraspherical{λ}})=2
 # return the space that has banded Conversion to the other
 function conversion_rule{aorder,border}(a::Ultraspherical{aorder},b::Ultraspherical{border})
     @assert domainscompatible(a,b)
-    
+
     aorder < border?a:b
 end
 
