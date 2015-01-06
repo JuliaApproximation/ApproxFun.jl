58d08f09f9f7c8eec665c093c5e376e1dadd70d0
Sheehan Olver <solver@mac.com>
2015-01-06 13:53:22 +1100

    Added Fun + Uniform Scaling

--- src/Fun/Fun.jl
+++ src/Fun/Fun.jl
@@ -137,6 +137,8 @@ for op = (:+,:-)
 
         ($op){N<:Number}(f::Fun,c::N)=$op(f,c*ones(f))
         ($op){N<:Number}(c::N,f::Fun)=$op(c*ones(f),f)    
+        ($op){S,T}(f::Fun{S,T},c::UniformScaling)=$op(f,c.λ)
+        ($op){S,T}(c::UniformScaling,f::Fun{S,T})=$op(c.λ,f)    
     end
 end 
 
