1e747dd77ca81528acd74f9ca15efed7d94757d5
Sheehan Olver <solver@mac.com>
2015-01-08 19:55:49 +1100

    FunctionSpace introspect

--- src/Plot/introspect.jl
+++ src/Plot/introspect.jl
@@ -1,21 +1,5 @@
 
-
-opcount(::Derivative)=1
-opcount(::ConstantOperator)=1
-opcount(::Conversion)=1
-opcount(::Multiplication)=1
-
-
-opcount(M::Union(MultiplicationWrapper,SpaceOperator,DiagonalArrayOperator))=1+opcount(M.op)
-opcount(A::Union(PlusOperator,TimesOperator,InterlaceOperator,DiagonalPiecewiseOperator,SumInterlaceOperator))=1+mapreduce(opcount,+,A.ops)
-
-
-add_edges!(D::Derivative,nd,M,labels)=(labels[nd]=string(nd)*":"*(D.order==1?"\$D":"\$D\^"*string(D.order))*"\$")
-add_edges!(A::ConstantOperator,nd,M,labels)=(labels[nd]=string(nd)*":"*string(A.c)*"I")
-add_edges!(A::Conversion,nd,M,labels)=(labels[nd]=string(nd)*":"*"C")
-add_edges!(A::Multiplication,nd,M,labels)=(labels[nd]=string(nd)*":"*"M")
-
-add_edges!(A::BandedOperator,nd,M,labels)=(labels[nd]=string(nd)*":"*string(typeof(A)))
+## General
 
 
 function treeadd_edges!(str,ops,node,M,labels)
@@ -25,24 +9,14 @@ function treeadd_edges!(str,ops,node,M,labels)
     for op in ops
         Main.Graphs.add_edge!(M,node,nd)
         add_edges!(op,nd,M,labels)
-        nd+=opcount(op)
+        nd+=treecount(op)
     end
 end
 
 
-for (WRAP,STR) in ((:SpaceOperator,:"Sw"),(:MultiplicationWrapper,:"Mw"),(:DiagonalArrayOperator,:"DiagArray"))
-    @eval add_edges!(A::$WRAP,nd,M,labels)=treeadd_edges!(string(nd)*":"*$STR,[A.op],nd,M,labels)
-end
-
-for (OP,STR) in ((:PlusOperator,:"+"),(:TimesOperator,:"*"),(:InterlaceOperator,:"Interlace"),
-    (:DiagonalPiecewiseOperator,:"DiagPiecewise"),(:SumInterlaceOperator,:"\$\\oplus\$Interlace"))
-    @eval add_edges!(A::$OP,nd,M,labels)=treeadd_edges!(string(nd)*":"*$STR,A.ops,nd,M,labels)
-end
-
-
-function introspect(A::BandedOperator)
+function introspect(A::Union(BandedOperator,Functional,FunctionSpace))
     require("TikzGraphs")
-    m=opcount(A)
+    m=treecount(A)
 
     M=Main.Graphs.simple_graph(m)
     labels=Array(ASCIIString,m)
@@ -52,3 +26,85 @@ function introspect(A::BandedOperator)
 
     Main.TikzGraphs.plot(M,labels)
 end
+
+
+
+## Spaces
+
+shortname(FS)=string(typeof(FS))
+shortname(CS::CosSpace)="Cos"
+shortname(CS::SinSpace)="Sin"
+shortname(CS::Chebyshev)="\$T\$"  #*string(first(domain(CS)))*","*string(last(domain(CS)))*"]"
+shortname{λ}(CS::Ultraspherical{λ})="\$U\^{"*string(λ)*"}\$"
+shortname(CS::Taylor)="\$H\^+\$"
+shortname(CS::Hardy{false})="\$H\^-\$"
+shortname(::SumSpace)="\$\\oplus\$"
+shortname(::PiecewiseSpace)="\$\\bigcup\$"
+shortname{T}(A::ArraySpace{T,1})="["*string(length(A))*"]"
+shortname{T}(A::ArraySpace{T,2})="["*string(size(A,1))*"\$\\times\$"*string(size(A,2))*"]"
+
+treecount(S::Union(SumSpace,PiecewiseSpace))=1+mapreduce(treecount,+,S.spaces)
+treecount(S::ArraySpace)=1+treecount(S.space)
+treecount(::FunctionSpace)=1
+
+
+add_edges!(FS::FunctionSpace,nd,M,labels)=(labels[nd]=string(nd)*":"*shortname(FS))
+
+for (OP) in (:SumSpace,:PiecewiseSpace)
+    @eval add_edges!(A::$OP,nd,M,labels)=treeadd_edges!(string(nd)*":"*shortname(A),A.spaces,nd,M,labels)
+end
+
+for (OP) in (:ArraySpace,)
+    @eval add_edges!(A::$OP,nd,M,labels)=treeadd_edges!(string(nd)*":"*shortname(A),[A.space],nd,M,labels)
+end
+
+
+
+
+## Operators
+
+
+treecount(::BandedOperator)=1
+
+treecount(::Derivative)=1
+treecount(::ConstantOperator)=1
+treecount(::Conversion)=1
+treecount(::Multiplication)=1
+
+
+treecount(M::Union(MultiplicationWrapper,DerivativeWrapper,IntegralWrapper,ConversionWrapper,SpaceOperator,DiagonalArrayOperator))=1+treecount(M.op)
+treecount(A::Union(PlusOperator,TimesOperator,InterlaceOperator,DiagonalPiecewiseOperator,SumInterlaceOperator))=1+mapreduce(treecount,+,A.ops)
+
+domainrangestr(A)=shortname(domainspace(A))*"\$\\rightarrow\$"*shortname(rangespace(A))
+
+
+shortname(A::BandedOperator)=string(typeof(A))
+shortname(D::Derivative)=(D.order==1?"\$D":"\$D\^"*string(D.order))*"\$:"*domainrangestr(D)
+shortname(A::ConstantOperator)=string(A.c)*"I"
+shortname(C::Conversion)="C:"*domainrangestr(C)
+shortname(A::Multiplication)=space(A.f)==domainspace(A)==rangespace(A)?"M["*shortname(space(A.f))*"]":"M["*shortname(space(A.f))*"]:"domainrangestr(A)
+
+shortname(A::SpaceOperator)="("*domainrangestr(A)*")"
+
+add_edges!(A::BandedOperator,nd,M,labels)=(labels[nd]=string(nd)*":"*shortname(A))
+
+
+
+
+
+for (WRAP,STR) in ((:MultiplicationWrapper,:"(M)"),(:DerivativeWrapper,:"(D)"),(:ConversionWrapper,"(C)"),(:DiagonalArrayOperator,:"DiagArray"))
+    @eval add_edges!(A::$WRAP,nd,M,labels)=treeadd_edges!(string(nd)*":"*$STR,[A.op],nd,M,labels)
+end
+
+
+
+@eval add_edges!(A::SpaceOperator,nd,M,labels)=treeadd_edges!(string(nd)*":"*shortname(A),[A.op],nd,M,labels)
+
+
+
+
+for (OP,STR) in ((:PlusOperator,:"+"),(:TimesOperator,:"*"),(:InterlaceOperator,:"Interlace"),
+    (:DiagonalPiecewiseOperator,:"DiagPiecewise"),(:SumInterlaceOperator,:"\$\\oplus\$"))
+    @eval add_edges!(A::$OP,nd,M,labels)=treeadd_edges!(string(nd)*":"*$STR,A.ops,nd,M,labels)
+end
+
