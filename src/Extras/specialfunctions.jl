cf9065ab9ee2aa4c79d1c7eef604ae5fe1fa1c40
Richard Mikael Slevinsky <Richard.Slevinsky@maths.ox.ac.uk>
2015-01-12 11:24:31 +0000

    More function overrides
    This covers about the first half of Julia’s intrinsic math functions.

--- src/Extras/specialfunctions.jl
+++ src/Extras/specialfunctions.jl
@@ -5,10 +5,10 @@ function splitatroots(f::Fun)
     d=domain(f)
 
     pts=roots(f)
-    
+
     if isempty(pts)
         f
-    else    
+    else
         da=first(d)
         isapprox(da,pts[1]) ? pts[1] = da : pts = [da,pts]
         db=last(d)
@@ -30,10 +30,10 @@ function Base.abs{S<:RealSpace,T<:Real}(f::Fun{S,T})
     d=domain(f)
 
     pts=roots(f)
-    
+
     if isempty(pts)
         sign(first(f))*f
-    else    
+    else
         splitmap(x->abs(f[x]),d,pts)
     end
 end
@@ -42,10 +42,10 @@ function Base.abs{S,T}(f::Fun{S,T})
     d=domain(f)
 
     pts=roots(f)
-    
+
     if isempty(pts)
         Fun(x->abs(f[x]),space(f))
-    else    
+    else
         splitmap(x->abs(f[x]),d,pts)
     end
 end
@@ -56,21 +56,21 @@ function Base.sign{S<:RealSpace,T<:Real}(f::Fun{S,T})
     d=domain(f)
 
     pts=roots(f)
-    
+
     if isempty(pts)
         sign(first(f))*one(T,f.space)
-    else    
+    else
         @assert isa(d,AffineDomain)
         da=first(d)
         isapprox(da,pts[1];atol=sqrt(eps(length(d)))) ? pts[1] = da : pts = [da,pts]
         db=last(d)
-        isapprox(db,pts[end];atol=sqrt(eps(length(d)))) ? pts[end] = db : pts = [pts,db]    
+        isapprox(db,pts[end];atol=sqrt(eps(length(d)))) ? pts[end] = db : pts = [pts,db]
         midpts = .5(pts[1:end-1]+pts[2:end])
         Fun([sign(f[midpts])],pts)
     end
 end
 
-# division by fun 
+# division by fun
 
 function ./{S,T,U,V}(c::Fun{S,T},f::Fun{U,V})
     r=roots(f)
@@ -93,11 +93,11 @@ function ./(c::Number,f::Fun{Chebyshev})
     fc = Fun(canonicalcoefficients(f),Interval())
     r = roots(fc)
     x = Fun(identity)
-    
+
     # if domain f is small then the pts get projected in
-    tol = 50eps()/length(domain(f))  
-    
-    
+    tol = 50eps()/length(domain(f))
+
+
     if length(r) == 0
         linsolve(Multiplication(f,space(f)),c;tolerance=tol)
     elseif length(r) == 1 && abs(abs(r[1]) - 1.) < tol
@@ -106,14 +106,14 @@ function ./(c::Number,f::Fun{Chebyshev})
             Fun(canonicalcoefficients(c./g),JacobiWeight(-1,0,domain(f)))
         else
             g = divide_singularity(1,fc)  # divide by 1-x
-            Fun(canonicalcoefficients(c./g),JacobiWeight(0,-1,domain(f)))                
-        end 
-    elseif length(r) ==2 && abs(r[1]+1) < tol && abs(r[2]-1) < tol                        
+            Fun(canonicalcoefficients(c./g),JacobiWeight(0,-1,domain(f)))
+        end
+    elseif length(r) ==2 && abs(r[1]+1) < tol && abs(r[2]-1) < tol
         g = divide_singularity(fc) # divide by 1-x^2
         # divide out singularities, tolerance needs to be chosen since we don't get
         # spectral convergence
         # TODO: switch to dirichlet basis
-        Fun(canonicalcoefficients(c./g),JacobiWeight(-1,-1,domain(f)))  
+        Fun(canonicalcoefficients(c./g),JacobiWeight(-1,-1,domain(f)))
     else
         #split at the roots
         c./splitatroots(f)
@@ -130,48 +130,20 @@ function ./{S<:MappedSpace}(c::Number,f::Fun{S})
     end
 end
 
-
-
-
-
-## We use \ as the Fun constructor might miss isolated features
-function Base.exp{S<:Ultraspherical}(f::Fun{S})
-    rf=chop(real(f),eps())
-
-    xm=rf.coefficients[1]==[0.]?first(domain(rf)):indmax(rf)    
-
-
-    B=Evaluation(space(f),xm)
-    D=Derivative(space(f))
-    A=[B,D-differentiate(f)]
-    A\[exp(f[xm]),0.]    
-end
-
-## Less accurate than solving differential equation with \
-for op in (:(Base.cos),:(Base.sin),:(Base.cospi),:(Base.sinpi),:(Base.sinc),:(Base.exp))
-    @eval begin
-        $op{S,T}(f::Fun{S,T})=Fun(x->($op)(f[x]),domain(f))  # The space might not be compatible
-    end
-end
-
-Base.tan(f::Fun)=sin(f)./cos(f)
-Base.csc(f::Fun)=1./sin(f)
-Base.sec(f::Fun)=1./cos(f)
-
 function .^{S<:MappedChebyshev}(f::Fun{S},k::Float64)
     fc = Fun(f.coefficients) #Project to interval
     x=Fun(identity)
 
     r = sort(roots(fc))
 
-    
+
     @assert length(r) <= 2
-    
+
     if length(r) == 0
         Fun(Fun(x->fc[x]^k).coefficients,space(f))
     elseif length(r) == 1
         @assert isapprox(abs(r[1]),1)
-        
+
         if isapprox(r[1],1.)
             Fun(coefficients(divide_singularity(+1,fc)^k),JacobiWeight(0.,k,space(f)))
         else
@@ -179,61 +151,137 @@ function .^{S<:MappedChebyshev}(f::Fun{S},k::Float64)
         end
     else
         @assert isapprox(r[1],-1)
-        @assert isapprox(r[2],1) 
-    
-        Fun(coefficients(divide_singularity(fc)^k),JacobiWeight(k,k,space(f)))  
+        @assert isapprox(r[2],1)
+
+        Fun(coefficients(divide_singularity(fc)^k),JacobiWeight(k,k,space(f)))
     end
 end
 
 Base.sqrt{S,T}(f::Fun{S,T})=f^0.5
+Base.cbrt{S,T}(f::Fun{S,T})=f^(1/3)
 
-
-
-
-
-## The following code will fail when xp and xm correspond to zeros of sin
-# for op = (:(Base.cos),:(Base.sin))
-#     @eval begin
-#         function ($op)(f::Fun)
-#             d=domain(f)
-#             D=diff(d)
-#             f2=f.*f
-#             xp=indmax(f2);xm=indmin(f2);
-#             B=[Evaluation(d,xm),Evaluation(d,xp)]
-#             fp=diff(f)
-#             fpp=diff(fp)
-#             [B,fp*D^2 - fpp*D + fp.^3]\[($op)(f[xm]),($op)(f[xp])]
-#         end
-#     end
-# end
-
-## The following backslash code works for real arguments but fails for complex Funs.
-
-
-Base.cos{S<:FunctionSpace{Float64},T<:Real}(f::Fun{S,T})=real(exp(im*f))
-Base.sin{S<:FunctionSpace{Float64},T<:Real}(f::Fun{S,T})=imag(exp(im*f))
-
-function pochhammer{T<:Number}(x::T,n::Integer)
-    ret = one(T)
-    if n>=0
-        for i=0:n-1
-            ret *= x+i
-        end
-    else
-        ret /= pochhammer(x+n,-n)
-    end
-    ret
+## We use \ as the Fun constructor might miss isolated features
+function Base.exp{S<:Ultraspherical,T}(f::Fun{S,T})
+    rf=chop(real(f),eps())
+    xmax=rf.coefficients==[0.]?last(domain(rf)):indmax(rf)
+    B=Evaluation(space(f),xmax)
+    D=Derivative(space(f))
+    A=[B,D-differentiate(f)]
+    (A\[1.])*exp(f[xmax])
 end
-function pochhammer{T<:Number}(x::Array{T},n::Integer)
-    ret = ones(T,size(x))
-    if n>=0
-        for i=0:n-1
-            ret .*= x+i
-        end
-    else
-        ret ./= pochhammer(x+n,-n)
-    end
-    ret
+Base.exp2(f::Fun) = exp(log(2)*f)
+Base.exp10(f::Fun) = exp(log(10)*f)
+
+function Base.log{S<:Ultraspherical,T}(f::Fun{S,T})
+    rf=chop(real(f),eps())
+    xmax=rf.coefficients==[0.]?last(domain(rf)):indmax(rf)
+    B=Evaluation(space(f),xmax)
+    D=Derivative(space(f))
+    A=[B,f*D]
+    A\[log(f[xmax]),differentiate(f)]
 end
-pochhammer{T1<:Number,T2<:Number}(x::T1,n::T2) = gamma(x+n)/gamma(x)
-pochhammer{T1<:Number,T2<:Number}(x::Array{T1},n::T2) = gamma(x+n)./gamma(x)
+Base.log2(f::Fun) = log(f)/log(2)
+Base.log10(f::Fun) = log(f)/log(10)
+
+for op in (:(Base.erf),:(Base.erfc))
+    @eval begin
+        function $op{S<:Ultraspherical,T}(f::Fun{S,T})
+            rf=chop(real(f),eps())
+            xmin=rf.coefficients==[0.]?first(domain(rf)):indmin(rf)
+            xmax=rf.coefficients==[0.]?last(domain(rf)):indmax(rf)
+            D=Derivative(space(f))
+            fp=differentiate(f)
+            fpp=differentiate(fp)
+            opfxmin,opfxmax = ($op)(f[xmin]),$(op)(f[xmax])
+            opmax = maxabs((opfxmin,opfxmax))
+            B=[Evaluation(space(f),xmin),Evaluation(space(f),xmax)]
+            ([B,fp*D^2+(2f*fp^2-fpp)*D]\[($op)(f[xmin])/opmax,$(op)(f[xmax])/opmax])*opmax
+        end
+    end
+end
+Base.erfi{S<:FunctionSpace{Float64},T<:Real}(f::Fun{S,T}) = imag(erf(im*f))
+Base.erfi(f::Fun) = -im*erf(im*f)
+
+function Base.erfcx{S<:Ultraspherical,T}(f::Fun{S,T})
+    rf=chop(real(f),eps())
+    xmin=rf.coefficients==[0.]?first(domain(rf)):indmin(rf)
+    D=Derivative(space(f))
+    fp=differentiate(f)
+    opfxmin = erfcx(f[xmin])
+    B=Evaluation(space(f),xmin)
+    ([B,D-2f*fp]\[1.0,-2fp/sqrt(π)/opfxmin])*opfxmin
+end
+
+function Base.dawson{S<:Ultraspherical,T}(f::Fun{S,T})
+    rf=chop(real(f),eps())
+    xmin=rf.coefficients==[0.]?first(domain(rf)):indmin(rf)
+    D=Derivative(space(f))
+    fp=differentiate(f)
+    opfxmin = dawson(f[xmin])
+    B=Evaluation(space(f),xmin)
+    ([B,D+2f*fp]\[1.0,fp/opfxmin])*opfxmin
+end
+
+Base.sin{S<:FunctionSpace{Float64},T<:Real}(f::Fun{S,T}) = imag(exp(im*f))
+Base.cos{S<:FunctionSpace{Float64},T<:Real}(f::Fun{S,T}) = real(exp(im*f))
+
+Base.sin(f::Fun) = (exp(im*f)-exp(-im*f))/2im
+Base.cos(f::Fun) = (exp(im*f)+exp(-im*f))/2
+Base.tan(f::Fun) = sin(f)/cos(f)     ##TODO: the spacepromotion doesn't work for tan for a domain including zeros of cos inside.
+
+Base.asin{S<:FunctionSpace{Float64},T<:Real}(f::Fun{S,T}) = imag(log(im*f+sqrt(1-f^2)))
+Base.acos{S<:FunctionSpace{Float64},T<:Real}(f::Fun{S,T}) = -imag(log(f-im*sqrt(1-f^2)))
+
+##TODO: If f and sqrt(1-f^2) are in different spaces, then the log's argument could be im*f⊕sqrt(1-f^2)... in the SumSpace. This also requires log for more spaces (or a constructor with edge detection).
+Base.asin(f::Fun) = -im*log(im*f+sqrt(1-f^2))
+Base.acos(f::Fun) = im*log(f-im*sqrt(1-f^2))
+#Base.atan(f::Fun) = asin(f/sqrt(f^2+1)) #This is inaccurate.
+
+Base.sinh(f::Fun) = (exp(f)-exp(-f))/2
+Base.cosh(f::Fun) = (exp(f)+exp(-f))/2
+#Base.tanh(f::Fun) = sinh(f)/cosh(f) #This is inaccurate.
+
+Base.asinh(f::Fun) = log(f+sqrt(f^2+1))
+Base.acosh(f::Fun) = log(f+sqrt(f+1)*sqrt(f-1))
+Base.atanh(f::Fun) = (log(1+f)-log(1-f))/2
+
+for (op,oprecip,opinv,opinvrecip) in ((:(Base.sin),:(Base.csc),:(Base.asin),:(Base.acsc)),
+                                      (:(Base.cos),:(Base.sec),:(Base.acos),:(Base.asec)),
+                                      (:(Base.tan),:(Base.cot),:(Base.atan),:(Base.acot)),
+                                      (:(Base.sinh),:(Base.csch),:(Base.asinh),:(Base.acsch)),
+                                      (:(Base.cosh),:(Base.sech),:(Base.acosh),:(Base.asech)),
+                                      (:(Base.tanh),:(Base.coth),:(Base.atanh),:(Base.acoth)))
+    @eval begin
+        $oprecip(f::Fun) = 1/$op(f)
+        $opinvrecip(f::Fun) = $opinv(1/f)
+    end
+end
+
+rad2deg(f::Fun) = 180*f/π
+deg2rad(f::Fun) = π*f/180
+
+for (op,opd,opinv,opinvd) in ((:(Base.sin),:(Base.sind),:(Base.asin),:(Base.asind)),
+                              (:(Base.cos),:(Base.cosd),:(Base.acos),:(Base.acosd)),
+                              (:(Base.tan),:(Base.tand),:(Base.atan),:(Base.atand)),
+                              (:(Base.sec),:(Base.secd),:(Base.asec),:(Base.asecd)),
+                              (:(Base.csc),:(Base.cscd),:(Base.acsc),:(Base.acscd)),
+                              (:(Base.cot),:(Base.cotd),:(Base.acot),:(Base.acotd)))
+    @eval begin
+        $opd(f::Fun) = $op(deg2rad(f))
+        $opinvd(f::Fun) = rad2deg($opinv(f))
+    end
+end
+
+#Won't get the zeros exactly 0 anyway so at least this way the length(coefficients) is smaller.
+Base.sinpi(f::Fun) = sin(π*f)
+Base.cospi(f::Fun) = cos(π*f)
+Base.sinc(f::Fun) = sin(π*f)/(π*f)
+Base.cosc(f::Fun) = cos(π*f)/f - sin(π*f)/(π*f^2)
+
+## Miscellaneous
+for op in (:(Base.expm1),:(Base.log1p),:(Base.atan),:(Base.tanh),
+           :(Base.erfinv),:(Base.erfcinv))
+    @eval begin
+        $op{S,T}(f::Fun{S,T})=Fun(x->($op)(f[x]),domain(f))
+    end
+end
