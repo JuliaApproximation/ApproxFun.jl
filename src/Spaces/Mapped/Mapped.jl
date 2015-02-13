d4cfc71cc536b32f13f285dbb25556d3c4d9a8c6
Sheehan Olver <solver@mac.com>
2015-02-13 11:21:45 +1100

    integrate over line with jacobi weight

--- src/Spaces/Mapped/Mapped.jl
+++ src/Spaces/Mapped/Mapped.jl
@@ -110,9 +110,17 @@ integrate{LS,T}(f::Fun{RaySpace{LS},T})=linsolve([BasisFunctional(1),Derivative(
 function integrate{LS<:JacobiWeight,RR<:Ray,T,TT,DS}(f::Fun{MappedSpace{LS,RR,TT,DS},T})
     # x^k -> x^(k+1)  so +1,-1 to singularities 
     # if the last entry of f is close to zero wei use the same space
-    D=Derivative(MappedSpace(Ray(),JacobiWeight(space(f).space.α+1,(space(f).space.β==0&&abs(last(f))≤1E-9)?0:(space(f).space.β-1),domain(space(f).space))))
+    D=Derivative(MappedSpace(space(f).domain,JacobiWeight(space(f).space.α+1,(space(f).space.β==0&&abs(last(f))≤1E-9)?0:(space(f).space.β-1),domain(space(f).space))))
     linsolve(D,f;tolerance=length(f)*1.0E-15)
 end
+
+function integrate{LS<:JacobiWeight,RR<:Line,T,TT,DS}(f::Fun{MappedSpace{LS,RR,TT,DS},T})
+    # x^k -> x^(k+1)  so +1,-1 to singularities 
+    # if the last entry of f is close to zero wei use the same space
+    D=Derivative(MappedSpace(space(f).domain,JacobiWeight(space(f).space.α-1,space(f).space.β-1,domain(space(f).space))))
+    linsolve([Evaluation(domainspace(D),0.),D],Any[0.,f];tolerance=length(f)*1.0E-15)
+end
+
 
 Base.cumsum{LS<:JacobiWeight,RR<:Ray,T,TT,DS}(f::Fun{MappedSpace{LS,RR,TT,DS},T})=integrate(f) # the choice of space is zero at 0
 
