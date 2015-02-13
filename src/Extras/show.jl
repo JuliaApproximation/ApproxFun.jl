10b568c0f9d852e06702b46711c4e079f9e9f1bd
Sheehan Olver <solver@mac.com>
2015-02-13 12:09:09 +1100

    Added MappedSpace and JacobiWeight

--- src/Extras/show.jl
+++ src/Extras/show.jl
@@ -1,4 +1,13 @@
 Base.show(io::IO,d::Interval)=print(io,"[$(d.a),$(d.b)]")
+function Base.show(io::IO,d::Line)
+    if d.centre == d.angle == 0 && d.α == d.β == -1.
+        print(io,"(-Inf,Inf)")
+    elseif  d.α == d.β == -1.
+        print(io,"Line($(d.centre),$(d.angle))")
+    else
+        print(io,"Line($(d.centre),$(d.angle),$(d.α),$(d.β))")
+    end        
+end
 Base.show(io::IO,d::PeriodicInterval)=print(io,"[$(d.a),$(d.b))")
 
 for typ in ("Chebyshev","Fourier","Laurent")
@@ -8,6 +17,24 @@ for typ in ("Chebyshev","Fourier","Laurent")
         show(io,domain(S))
         print(io,")")
     end
+end
+
+function Base.show(io::IO,s::JacobiWeight)
+    d=domain(s)
+    #TODO: Get shift and weights right
+    if s.α==s.β
+        print(io,"(1-x^2)^$(s.α)")    
+    else
+        print(io,"(1+x)^$(s.α)*(1-x)^$(s.β)*")
+    end
+    
+    show(io,s.space)
+end
+
+function Base.show(io::IO,s::MappedSpace)
+    show(io,s.space)
+    print(io,"↦")
+    show(io,s.domain)
 end
 
 function Base.show{S}(io::IO,s::ArraySpace{S,1})
