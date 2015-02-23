4c26446237a8d13dd02d6c81bdf5076d2df8c752
Sheehan Olver <solver@mac.com>
2015-02-23 11:04:02 +1100

    Removed divide_singularity(::Vector) so all calls are ::Fun, which know the space

--- src/Spaces/Singularities/divide_singularity.jl
+++ src/Spaces/Singularities/divide_singularity.jl
@@ -1,70 +1,20 @@
-## This file has methods to stably divide the singularity
 
 
-# function dirichlet_transform{T<:Number}(s,v::Vector{T})
-#     n=length(v)
-#     w=zeros(T,n-1)
-#     w[n-1]=v[n]
-#     for k=n-2:-1:1
-#         @inbounds w[k]=v[k+1] + s.*w[k+1] 
-#     end
-#     
-#    @assert abs(s.*w[1] + v[1]) < 10000*n*eps()
-#     
-#     w
-# end
-# 
-# function idirichlet_transform{T<:Number}(s,v::Vector{T})
-#     n=length(v)
-#     w=zeros(T,n+1)
-#     w[1]=-s.*v[1]
-#     for k=2:n
-#         @inbounds w[k]=v[k-1] - s.*v[k] 
-#     end
-#     
-#     w[n+1]=v[n]
-#     
-#     w
-# end
-
-# 
-# function dirichletrange_divide_singularity{T<:Number}(s,v::Vector{T})
-#     n=length(v)
-#     w=zeros(T,n)
-#     w[n]=-2s*v[n]
-#     
-#     for k=n-1:-1:2
-#         @inbounds w[k]=-2s*(v[k] - .5w[k+1])
-#     end
-#     
-#     w[1]=-s*v[1]
-#     if n≥2
-#         w[1]+=0.5s*w[2]
-#     end
-#     
-#     w    
-# end
-# 
-# function dirichlet_divide_singularity{T<:Number}(s,v::Vector{T})
-#     n=length(v)
-#     w=zeros(T,n-1)
-#     w[n-1]=-2s*v[n]
-#     w[n-2]=-2s*(v[n-1]-w[n-1])
-#     
-#     for k=n-3:-1:1
-#         @inbounds w[k]=-2s*(v[k+1] - w[k+1] + .5s*w[k+2])
-#     end
-#     
-#    @assert abs(1.5w[1] - .5w[2] - v[1]) < 10000n*eps()
-#     
-#     w    
-# end
+## This file has methods to stably divide the jacobi singularity
 
 
 function dirichlet_divide_singularity{T<:Number}(b::Bool,v::Vector{T})
     n=length(v)
     w=zeros(T,n-1)
-    s=b?1:-1
+    s=b?1:-1    
+    
+    if n==1
+        return w
+    elseif n==2
+        w[1]=-s*v[2]    
+        return w
+    end
+    
     w[n-1]=-2s*v[n]
     
     for k=n-2:-1:2
@@ -78,36 +28,6 @@ function dirichlet_divide_singularity{T<:Number}(b::Bool,v::Vector{T})
 end
 
 
-
-#divide_singularity(s,v::Vector)=dirichletrange_divide_singularity(s,dirichlet_transform(s,v))
-#divide_singularity(s,v::Vector)=idirichlet_transform(s,dirichlet_transform(s,dirichletrange_divide_singularity(s,dirichlet_transform(s,v))))
-#divide_singularity(s,f::Fun)=Fun(divide_singularity(s,f.coefficients),f.space)
-
-
-
-
-# 
-# 
-# ## both left and right dirichlet_transform
-# converts to f_0 T_0 + f_1 T_1 +  \sum f_k (T_k - T_{k-2})
-# function dirichlettransform!(w::Vector)
-#     for k=length(w)-2:-1:1
-#         @inbounds w[k] += w[k+2] 
-#     end
-#     
-#     w
-# end
-# 
-# function idirichlettransform!(w::Vector)
-#     for k=3:length(w)
-#         @inbounds w[k-2]-= w[k] 
-#     end
-#     
-#     w
-# end
-# 
-# dirichlettransform(v::Vector)=dirichlettransform!(copy(v))
-# idirichlettransform(v::Vector)=idirichlettransform!(copy(v))
 
 
 
@@ -138,11 +58,8 @@ function dirichlet_divide_singularity{T<:Number}(v::Vector{T})
 end
 
 
-divide_singularity(s::Bool,v::Vector)=dirichlet_divide_singularity(s,spaceconversion(v,Chebyshev,s?ChebyshevDirichlet{0,1}:ChebyshevDirichlet{1,0}))
-divide_singularity(s::Integer,v::Vector)=divide_singularity(s==1,v)
+divide_singularity(s::Bool,f::Fun)=Fun(dirichlet_divide_singularity(s,Fun(f,s?ChebyshevDirichlet{0,1}:ChebyshevDirichlet{1,0}).coefficients),Chebyshev(domain(f)))
+divide_singularity(s::Integer,f::Fun)=divide_singularity(s==1,f)
 
-divide_singularity(v::Vector)=dirichlet_divide_singularity(spaceconversion(v,Chebyshev,ChebyshevDirichlet{1,1}))
+divide_singularity(f::Fun)=Fun(dirichlet_divide_singularity(Fun(f,ChebyshevDirichlet{1,1}).coefficients),Chebyshev(domain(f)))
 
-
-divide_singularity{T}(f::Fun{Chebyshev,T})=Fun(divide_singularity(f.coefficients),f.space)
-divide_singularity{T}(s,f::Fun{Chebyshev,T})=Fun(divide_singularity(s,f.coefficients),f.space)
