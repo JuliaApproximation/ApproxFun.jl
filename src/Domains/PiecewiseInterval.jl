ce966dd73bb40688838f9317456b094dd959946d
Sheehan Olver <solver@mac.com>
2015-03-23 21:24:19 +1100

    Added type to union domain

--- src/Domains/PiecewiseInterval.jl
+++ src/Domains/PiecewiseInterval.jl
@@ -1,5 +1,6 @@
 using ApproxFun
-    import ApproxFun:canonicalspace,spacescompatible,points,RealBasis,transform
+    import ApproxFun:canonicalspace,spacescompatible,points,RealBasis,transform,checkpoints,pieces,canonicalspace,spacescompatible,
+                    domainscompatible,conversion_rule
 
 
 immutable PiecewiseInterval{T<:Number} <:Domain{T}
@@ -7,6 +8,7 @@ immutable PiecewiseInterval{T<:Number} <:Domain{T}
 end
 
 PiecewiseInterval(d::Number...)=PiecewiseInterval([d...])
+==(a::PiecewiseInterval,b::PiecewiseInterval)=a.points==b.points
 
 Base.length(d::PiecewiseInterval)=length(d.points)-1
 Base.getindex(d::PiecewiseInterval,j::Integer)=Interval(d.points[j],d.points[j+1])
@@ -19,17 +21,20 @@ function points(d::PiecewiseInterval,n)
         vcat([points(d[j],k) for j=r+1:length(d)]...)]
 end
 
+pieces(d::PiecewiseInterval,k)=d[k]
+pieces{T}(d::PiecewiseInterval{T})=Interval{T}[d[k] for k=1:length(d)]
 
 Base.rand(d::PiecewiseInterval)=rand(d[rand(1:length(d))])
-checkpoints{T}(d::PiecewiseInterval{T})=union(T[checkpoints(d[k]) for k=1:length(d)])
-
+checkpoints{T}(d::PiecewiseInterval{T})=mapreduce(checkpoints,union,pieces(d))
+Base.first(d::PiecewiseInterval)=first(d.points)
 
 
 immutable ContinuousSpace <: FunctionSpace{RealBasis,UnionDomain}
     domain::PiecewiseInterval
 end
 
-
+spacescompatible(a::ContinuousSpace,b::ContinuousSpace)=domainscompatible(a,b)
+conversion_rule(a::ContinuousSpace,b::PiecewiseSpace{ChebyshevDirichlet{1,1},RealBasis})=a
 
 function transform(S::ContinuousSpace,vals::Vector)
     n=length(vals)
@@ -39,64 +44,31 @@ function transform(S::ContinuousSpace,vals::Vector)
 
     PT=promote_type(eltype(d),eltype(vals))
     if k==0
-        ret=Array(PT,n)
-        for j=1:n
-            ret[j]=transform(ChebyshevDirichlet{1,1}(d[j]),[vals[j]])[1]
+        vals
+    else
+        ret=Array(PT,n-K+1)
+        r=n-K*k
+        j=1
+        cfs=transform(ChebyshevDirichlet{1,1}(d[j]),vals[(j-1)*(k+1)+1:j*(k+1)])
+        ret[1]=cfs[1]-cfs[2]
+        ret[2]=cfs[1]+cfs[2]
+        ret[K+2:K:end]=cfs[3:end]
+        for j=2:r
+            cfs=transform(ChebyshevDirichlet{1,1}(d[j]),vals[(j-1)*(k+1)+1:j*(k+1)])
+            ret[j+1]=cfs[1]+cfs[2]
+            ret[K+j+1:K:end]=cfs[3:end]
+        end
+
+        for j=r+1:K
+            cfs=transform(ChebyshevDirichlet{1,1}(d[j]),vals[r*(k+1)+(j-r-1)*k+1:r*(k+1)+(j-r)*k])
+            ret[j+1]=cfs[1]+cfs[2]
+            ret[K+j+1:K:end]=cfs[3:end]
         end
 
         ret
-    else
-        ret=zeros(PT,K+1+K*(k-1))
-
-
-
-
-
-        r=n-K*k
-        M=Array(PT,k+1,K)
-
-        for j=1:r
-            M[:,j]=transform(ChebyshevDirichlet{1,1}(d[j]),vals[(j-1)*(k+1)+1:j*(k+1)])
-        end
-        for j=r+1:length(d)
-            M[1:k,j]=transform(ChebyshevDirichlet{1,1}(d[j]),vals[r*(k+1)+(j-r-1)*k+1:r*(k+1)+(j-r)*k])
-            M[k+1,j]=zero(PT)
-        end
-
-        vec(M.')
     end
 end
 
-
-d=PiecewiseInterval(1.,2.,3.)
-S=ContinuousSpace(d)
+canonicalspace(S::ContinuousSpace)=PiecewiseSpace(map(ChebyshevDirichlet{1,1},pieces(domain(S))))
 
 
-exp(1.),exp(2.)
-cfs[1]-cfs[2],cfs[1]+cfs[2]
-
-cfs=Fun(f,ChebyshevDirichlet{1,1}([1.,2.])).coefficients
-
-f=x->exp(x)
-vals=map(f,points(d,100))
-n=length(vals)
-d=domain(S)
-K=length(d)
-k=div(n,K)
-
-
-PT=Float64
-ret=zeros(PT,K+1+K*(k-1))
-#.'
-j=1
-cfs=transform(ChebyshevDirichlet{1,1}(d[j]),vals[(j-1)*(k+1)+1:j*(k+1)])
-ret[1]=cfs[1]-cfs[2]
-ret[2]=cfs[1]+cfs[2]
-ret[3:k+1]=cfs[3:end]
-
-        for j=1:r
-M[:,j]=transform(ChebyshevDirichlet{1,1}(d[j]),vals[(j-1)*(k+1)+1:j*(k+1)])
-end
-
-r=n-K*k
-ret
--- src/Domains/UnionDomain.jl
+++ src/Domains/UnionDomain.jl
@@ -2,17 +2,19 @@
 
 export UnionDomain
 
-immutable UnionDomain{D<:Domain} <:Domain
+immutable UnionDomain{D<:Domain,T<:Number} <:Domain{T}
     domains::Vector{D}
 end
+
+UnionDomain{D<:Domain}(d::Vector{D})=UnionDomain{D,mapreduce(eltype,promote_type,d)}(d)
 
 
 
 ∪{D<:Domain}(d::Vector{D}) = UnionDomain(d)
-∪(d1::UnionDomain,d2::UnionDomain)=UnionDomain([d1.domains,d2.domains])
-∪(d1::Domain,d2::UnionDomain)=UnionDomain([d1,d2.domains])
-∪(d1::UnionDomain,d2::Domain)=UnionDomain([d1.domains,d2])
-∪(d1::Domain,d2::Domain)=UnionDomain([d1,d2])
+∪{D1,D2,T1,T2}(d1::UnionDomain{D1,T1},d2::UnionDomain{D2,T2})=UnionDomain([d1.domains,d2.domains])
+∪{T1,D2,T2}(d1::Domain{T1},d2::UnionDomain{D2,T2})=UnionDomain([d1,d2.domains])
+∪{D1,T1,T2}(d1::UnionDomain{D1,T1},d2::Domain{T2})=UnionDomain([d1.domains,d2])
+∪{T1,T2}(d1::Domain{T1},d2::Domain{T2})=UnionDomain([d1,d2])
 Base.length(d::UnionDomain)=d.domains|>length
 Base.getindex(d::UnionDomain,k)=d.domains[k]
 for op in (:(Base.first),:(Base.last))
