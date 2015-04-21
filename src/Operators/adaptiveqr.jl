4ada9f61168ed0698964beff573a9839531e6fe9
Sheehan Olver <solver@mac.com>
2015-04-21 14:38:35 +1000

    Added warning about #bcs

--- src/Operators/adaptiveqr.jl
+++ src/Operators/adaptiveqr.jl
@@ -194,7 +194,7 @@ function adaptiveqr!(B::AlmostBandedOperator,v::Array,tol::Real,N)
     end
 
     if j >= N
-        warn("Maximum number of iterations " * string(N) * " reached")
+        warn("Maximum number of iterations " * string(N) * " reached.  Check that the correct number of boundary conditions are specified, or change maxiteration.")
     end
 
     ##TODO: why max original length?
