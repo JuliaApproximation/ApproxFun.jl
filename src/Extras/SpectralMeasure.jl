e7f3ce5cd4ded65026da61d49f1f357cfab9d175
Sheehan Olver <solver@mac.com>
2015-05-08 16:41:16 +1000

    spectral measure routine

--- src/Extras/SpectralMeasure.jl
+++ src/Extras/SpectralMeasure.jl
@@ -1,20 +1,37 @@
 using ApproxFun
 
-function Jacobi(a,b,N)
-    J = zeros(N,N)
-    J[1,1]=a[1]
-    n=length(a)
-    for i = 1:n-1
-        J[i+1,i+1] = a[i+1]
-        J[i,i+1] = b[i]
-        J[i+1,i] = b[i]
-    end
-    for i = n:N-1
-        J[i,i+1] = 1
-        J[i+1,i] = 1
-    end
-    J
+function spectralmeasure(a,b)
+    T,K=tkoperators(a,b)
+
+  L = T+K
+  Ti=inv(T)
+  m=size(K.matrix,1)
+  IKin=CompactOperator(inv(full((I+K*Ti)[1:m,1:m]))-eye(m))+I
+  Lin=Ti*IKin
+
+  x=Fun(identity,[-2,2])
+  wΔ=sqrt(4-x^2);wΔ=wΔ/sum(wΔ)
+  Fun(Fun(Lin*[1],Ultraspherical{1}([-2,2])),Chebyshev)*wΔ
 end
+
+function tkoperators(a,b)
+    assert(length(a)-length(b)==1)
+    n = length(a)
+    L = Lmatrix(a,b,2n)
+
+    T=ToeplitzOperator(vec(L[2*n,2*n-1:-1:1]),[L[n,n]])
+    K = zeros(2*n,2*n)
+    for i = 1:2*n
+        for j = 1:i
+            K[i,j] = L[i,j]-T[i,j]
+        end
+    end
+
+    K = CompactOperator(K)
+    T,K
+end
+
+
 
 function Lmatrix(a,b,N)
   # initial values
@@ -22,6 +39,8 @@ function Lmatrix(a,b,N)
   L[1,1] = 1
   L[2,1] = -a[1]/b[1]
   L[2,2] = 1/b[1]
+
+  n = length(a)
   # the generic case.
   for i = 3:n
       L[i,1] = (L[i-1,2]-a[i-1]*L[i-1,1]-b[i-2]*L[i-2,1])/b[i-1]
@@ -45,112 +64,88 @@ function Lmatrix(a,b,N)
   L
 end
 
-function Lop(a,b)
+function jacobimatrix(a,b,N)
+    J = zeros(N,N)
+    J[1,1]=a[1]
     n=length(a)
-    L = Lmatrix(a,b,2*n)
-
-    T=ToeplitzOperator(vec(L[2*n,2*n-1:-1:1]),[L[n,n]])
-    K = zeros(2*n,2*n)
-    for i = 1:2*n
-      for j = 1:i
-        K[i,j] = L[i,j]-T[i,j]
-      end
+    for i = 1:n-1
+        J[i+1,i+1] = a[i+1]
+        J[i,i+1] = b[i]
+        J[i+1,i] = b[i]
     end
-    K = CompactOperator(K)
-    T+K
+    for i = n:N-1
+        J[i,i+1] = 1
+        J[i+1,i] = 1
+    end
+    J
 end
 
 
+T,K=tkoperators([0.,0.],[2.])
+joukowsky(z)=z+1./z
+λ=real(joukowsky(complexroots(Fun(T))))
 
+λ[1]
 
-function Base.inv(T::ToeplitzOperator)
-    @assert length(T.nonnegative)==1
-    ai=T\[1.0]
-    ToeplitzOperator(ai[2:end],ai[1:1])
+function jacobioperator(a,b)
+    Δ=ToeplitzOperator([1.],[0.,1.])
+    B=jacobimatrix(a,b-1,length(a))
+    Δ+CompactOperator(B)
 end
 
 
-    n=3
-    a = 0.1rand(n)
-    b = 1+0.01rand(n-1)
-    J = Jacobi(a,b,4n)
-    L=Lop(a,b)
-    T=L.ops[1]
-    T,K=L.ops
-    Ti=inv(T)
-    m=size(K.matrix,1)
-    IKin=CompactOperator(inv(full((I+K*Ti)[1:m,1:m]))-eye(m))+I
-    Lin=Ti*IKin
+a,b=[2.,0.],[1.]
+    J=jacobioperator(a,b)
+    T,K=tkoperators(a,b)
 
-(Lin*L)[1:10,1:10]|>chopm
+λ=joukowsky(complexroots(Fun(T)))|>real
+n=20;    A=J[1:n,1:n]|>full
+    L=A-λ[1]*I|>qlgivens!
+    Q=(A-λ[1]*I)*inv(L)
+    Q*Q'
+L*A*inv(L)|>chopm
 
-x=Fun(identity,[-2,2])
-    wΔ=sqrt(4-x^2);wΔ=wΔ/sum(wΔ)
-    w=Fun(Fun(Lin*[1],Ultraspherical{1}([-2,2])),Chebyshev)*wΔ
+w=spectralmeasure([0.5,0.],[1.])
+
+ApproxFun.plot(w)
+
+J=jacobioperator([0.,0.],[2.])
+n=200;    A=J[1:n,1:n]|>full
+    L=A|>copy|>qlgivens!
+    L*A*inv(L)|>chopm
+
+Δ=ToeplitzOperator([1.],[0.,1.])
+    B=jacobimatrix([0.,0.],[2.-1.],2)
+    J=Δ+CompactOperator(B)
+    J[1:10,1:10]
+
+
+jacobimatrix([0.,0.],[2.],100)|>eigvals
+T[1:10,1:10]|>full|>inv
+
+
+
+
+
+
+x=Fun(identity,[-2,2]);w=1/sqrt(4-x^2)
+    lanczos(w,10)
+
+
+wL=Fun(1,[-2,2]);wL=wL/sum(wL)
+
+@code_typed spectralmeasure(zeros(n),lanczos(wL,n-1)[2])
+
+m=length(w)
+
+n=60; @time w=spectralmeasure(0.01(rand(n)-0.5),ones(n-1))
     ApproxFun.plot(w)
 
-a
+(lanczos(w,n-1)[2]-lanczos(wL,n-1)[2])|>norm
 
-lanczos(w,3)[1]-a
+
+length(w)
+lanczos(w,3)
 lanczos(w,3)[2]-[b;1]
 
 
-b
-
-ApproxFun.plot(w)
-
-S=space(w);weight(S,pts).*itransform(S.space,cfs)
-points(w)
-values(w)
-
-
-ApproxFun.plot(w)
-
-
-
-sqrt(4-x^2)
-
-
-I
-
-inv(T)
-
-ai
-(T\[1.0])-ai.coefficients
-
-Multiplication(a,space(a))[1:10,1:10]
-
-
-a
-ai=Multiplication(a,space(a))\1
-
-
-inv(T)
-
-
-Li[1:10,1:10]*T[1:10,1:10]
-
-
-
-complexroots(a)
-
-ToeplitzOperator(a)
-
-
-
-
-
-inv(L)
-
-
-
-
-
-
-
-[1:4n,1:4n]|>full
-    inv(L)*J*L
-
-
-
-eigvals(J)
