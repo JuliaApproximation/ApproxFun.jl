b93042294f969a4132dd3a0e24b1b6378608c797
Sheehan Olver <solver@mac.com>
2015-05-15 19:07:41 +1000

    dekron for ConversionWrapper and TimesOperator

--- src/PDE/KroneckerOperator.jl
+++ src/PDE/KroneckerOperator.jl
@@ -98,13 +98,17 @@ KroneckerOperator(A::Fun,B)=KroneckerOperator(Multiplication(A),B)
 
 isproductop(::)=false
 isproductop(::KroneckerOperator)=true
-isproductop(sp::SpaceOperator)=isproductop(sp.op)
-isproductop(sp::ConstantTimesOperator)=isproductop(sp.op)
+
+isproductop(A::Union(SpaceOperator,ConstantTimesOperator,ConversionWrapper))=isproductop(A.op)
+isproductop(A::TimesOperator)=all(isproductop,A.ops)
 isproductop{V,T<:AbstractArray}(::ConstantOperator{V,T})=true
 
 #dekron gives the operators that make up a productop
 
 dekron(K::KroneckerOperator,k)=K.ops[k]
+
+dekron(S::ConversionWrapper,k)=dekron(S.op,k)
+dekron(S::TimesOperator,k)=*(map(op->dekron(op,k),S.ops)...)
 dekron(S::SpaceOperator,k)=SpaceOperator(dekron(S.op,k),domainspace(S)[k],rangespace(S)[k])
 #TODO: dekron(S::SpaceOperator,k)=SpaceOperator(dekron(S.op,k),domainspace(S)[k],rangespace(S)[k])
 dekron(sp::ConstantTimesOperator,k)=k==1?sp.c*dekron(sp.op,k):dekron(sp.op,k)
