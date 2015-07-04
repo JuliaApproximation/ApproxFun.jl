49ff87cf9494e9b59540bebb5272bf52370ba3de
Sheehan Olver <solver@mac.com>
2015-07-04 10:35:22 +1000

    Pass options to Gadfly

--- src/Plot/Gadfly.jl
+++ src/Plot/Gadfly.jl
@@ -28,9 +28,9 @@ end
 
 gadflyopts(opts...)=gadflyopts(;opts...)
 
-function gadflyplot{T<:Real}(xx::Vector{T},yy::Vector;opts...)
+function gadflyplot{T<:Real}(xx::Vector{T},yy::Vector,v...;opts...)
     require("Gadfly")
-    Main.Gadfly.plot(x=xx, y=yy,gadflyopts(opts...)...)
+    Main.Gadfly.plot(x=xx, y=yy,v...,gadflyopts(opts...)...)
 end
 
 function gadflylayer{T<:Real}(xx::Vector{T},yy::Vector)
@@ -39,12 +39,12 @@ function gadflylayer{T<:Real}(xx::Vector{T},yy::Vector)
 end
 
 
-function gadflyplot{T<:Complex}(xx::Vector{T},yy::Vector;opts...)
+function gadflyplot{T<:Complex}(xx::Vector{T},yy::Vector,v...;opts...)
     warn("Complex domain, projecting to real axis")
-    gadflyplot(real(xx),yy;opts...)
+    gadflyplot(real(xx),yy,v...;opts...)
 end
 
-function gadflyplot{T<:Real}(x::Vector{T},y::Vector{Complex{Float64}};opts...)
+function gadflyplot{T<:Real}(x::Vector{T},y::Vector{Complex{Float64}},v...;opts...)
     require("Gadfly")
     require("DataFrames")
     r=real(y)
@@ -53,12 +53,12 @@ function gadflyplot{T<:Real}(x::Vector{T},y::Vector{Complex{Float64}};opts...)
     dat=Main.DataFrames.DataFrame(Any[[x,x],[r,i],[fill("Re",length(x)),fill("Im",length(x))]],Main.DataFrames.Index((@compat Dict(:x=>1,:y=>2,:Function=>3)),
             [:x,:y,:Function]))
 
-    Main.Gadfly.plot(dat,x="x",y="y",color="Function",gadflyopts(opts...)...)
+    Main.Gadfly.plot(dat,x="x",y="y",color="Function",v...,gadflyopts(opts...)...)
 end
 
 #Plot multiple contours
 # columns are plots
-function gadflyplot{T<:Real,V<:Real}(x::Matrix{T},y::Matrix{V};opts...)
+function gadflyplot{T<:Real,V<:Real}(x::Matrix{T},y::Matrix{V},v...;opts...)
     require("Gadfly")
     require("DataFrames")
 
@@ -66,13 +66,13 @@ function gadflyplot{T<:Real,V<:Real}(x::Matrix{T},y::Matrix{V};opts...)
                                   Main.DataFrames.Index((@compat Dict(:x=>1,:y=>2,:Function=>3)),
             [:x,:y,:Function]))
 
-    Main.Gadfly.plot(dat,x="x",y="y",color="Function",gadflyopts(opts...)...)
+    Main.Gadfly.plot(dat,x="x",y="y",color="Function",v...,gadflyopts(opts...)...)
 end
 
 
 
 
-function gadflycontour(x::Vector,y::Vector,z::Matrix;levels=-1,axis=-1)
+function gadflycontour(x::Vector,y::Vector,z::Matrix,v...;levels=-1,axis=-1)
     require("Gadfly")
     if axis==-1
         axis=[x[1],x[end],y[1],y[end]]
@@ -81,12 +81,12 @@ function gadflycontour(x::Vector,y::Vector,z::Matrix;levels=-1,axis=-1)
     if levels==-1
         Main.Gadfly.plot(x=x,y=y,z=z,Main.Gadfly.Geom.contour,
                     Main.Gadfly.Scale.x_continuous(minvalue=axis[1],maxvalue=axis[2]),
-                    Main.Gadfly.Scale.y_continuous(minvalue=axis[3],maxvalue=axis[4]))
+                    Main.Gadfly.Scale.y_continuous(minvalue=axis[3],maxvalue=axis[4]),v...)
     else
         Main.Gadfly.plot(x=x,y=y,z=z,
                             Main.Gadfly.Geom.contour(levels=levels),
                             Main.Gadfly.Scale.x_continuous(minvalue=axis[1],maxvalue=axis[2]),
-                            Main.Gadfly.Scale.y_continuous(minvalue=axis[3],maxvalue=axis[4]))
+                            Main.Gadfly.Scale.y_continuous(minvalue=axis[3],maxvalue=axis[4]),v...)
     end
 end
 
@@ -101,12 +101,12 @@ function gadflycontourlayer(x::Vector,y::Vector,z::Matrix;levels=-1)
     end
 end
 
-function dotplot{T<:Real,V<:Real}(x::Vector{T},y::Vector{V};axis=-1)
+function dotplot{T<:Real,V<:Real}(x::Vector{T},y::Vector{V},v...;axis=-1)
     require("Gadfly")
     if axis==-1
-        Main.Gadfly.plot(x=x,y=y)
+        Main.Gadfly.plot(x=x,y=y,v...)
     else
-        Main.Gadfly.plot(x=x,y=y,Main.Gadfly.Scale.y_continuous(minvalue=axis[1],maxvalue=axis[2]))
+        Main.Gadfly.plot(x=x,y=y,Main.Gadfly.Scale.y_continuous(minvalue=axis[1],maxvalue=axis[2]),v...)
     end
 end
 dotplot{T<:Number}(x::Vector{T})=dotplot(real(x),imag(x))
--- src/Plot/Plot.jl
+++ src/Plot/Plot.jl
@@ -65,11 +65,11 @@ function layer(opts...)
     end
 end
 
-function contour(x,y::Vector,z::Array;opts...)
+function contour(x,y::Vector,z::Array,v...;opts...)
     if plotter[:contour]=="Gadfly"
-        gadflycontour(x,y,z;opts...)
+        gadflycontour(x,y,z,v...;opts...)
     elseif plotter[:contour]=="PyPlot"
-        pycontour(x,y,z;opts...)
+        pycontour(x,y,z,v...;opts...)
     else
         error("Plotter " * plotter[:contour] * " not supported.")
     end
@@ -92,20 +92,20 @@ end
 
 for OP in (:plot,:layer)
     @eval begin
-        function $OP{S,T<:Real}(f::Fun{S,T};opts...)
+        function $OP{S,T<:Real}(f::Fun{S,T},v...;opts...)
             f=pad(f,3length(f)+50)
-            $OP(points(f),values(f);opts...)
+            $OP(points(f),values(f),v...;opts...)
         end
 
-        function $OP{S,T<:Complex}(f::Fun{S,T};opts...)
+        function $OP{S,T<:Complex}(f::Fun{S,T},v...;opts...)
             f=pad(f,3length(f)+50)
-            $OP(points(f),values(f);opts...)
+            $OP(points(f),values(f),v...;opts...)
         end
 
-        function $OP{F<:Fun}(f::Vector{F};opts...)
+        function $OP{F<:Fun}(f::Vector{F},v...;opts...)
             if plotter[:plot]=="PyPlot"
                 for k=1:length(f)
-                    $OP(f[k];opts...)
+                    $OP(f[k],v...;opts...)
                 end
             else
                 n=3mapreduce(length,max,f)+50
@@ -115,57 +115,57 @@ for OP in (:plot,:layer)
                     X[:,k]=points(space(f[k]),n)
                     Y[:,k]=values(pad(f[k],n))
                 end
-                plot(X,Y;opts...)
+                plot(X,Y,v...;opts...)
             end
         end
 
-        function $OP{S}(r::Range,f::Fun{S,Float64};opts...)
-            $OP([r],f[[r]];opts...)
+        function $OP{S}(r::Range,f::Fun{S,Float64},v...;opts...)
+            $OP([r],f[[r]],v...;opts...)
         end
     end
 end
 
-function complexplot(f::Fun;opts...)
+function complexplot(f::Fun,v...;opts...)
     f=pad(f,3length(f)+50)
     vals =values(f)
     d = domain(f)
     if isa(d,Circle)
-        plot(real([vals,vals[1]]),imag([vals,vals[1]]);opts...)
+        plot(real([vals,vals[1]]),imag([vals,vals[1]]),v...;opts...)
     else
-        plot(real(vals),imag(vals);opts...)
+        plot(real(vals),imag(vals),v...;opts...)
     end
 end
 
-function complexplot{F<:Fun}(f::Vector{F};opts...)
+function complexplot{F<:Fun}(f::Vector{F},v...;opts...)
     for k=1:length(f)
-        complexplot(f[k];opts...)
+        complexplot(f[k],v...;opts...)
     end
 end
 
-function complexlayer(f::Fun;opts...)
+function complexlayer(f::Fun,v...;opts...)
     f=pad(f,3length(f)+50)
     vals =values(f)
     d = domain(f)
     if isa(d,Circle)
-        layer(real([vals,vals[1]]),imag([vals,vals[1]]);opts...)
+        layer(real([vals,vals[1]]),imag([vals,vals[1]]),v...;opts...)
     else
-        layer(real(vals),imag(vals);opts...)
+        layer(real(vals),imag(vals),v...;opts...)
     end
 end
 
-function complexlayer{F<:Fun}(f::Vector{F};opts...)
-    typeof(complexlayer(f[1];opts...)[1])[complexlayer(f[k];opts...)[1] for k=1:length(f)]
+function complexlayer{F<:Fun}(f::Vector{F},v...;opts...)
+    typeof(complexlayer(f[1],v...;opts...)[1])[complexlayer(f[k],v...;opts...)[1] for k=1:length(f)]
 end
 
 for (plt,TYP) in ((:plot,:Real),(:complexplot,:Complex),(:layer,:Real),(:complexlayer,:Complex))
-    @eval $plt{S<:Union(PiecewiseSpace,ArraySpace),T<:$TYP}(f::Fun{S,T};opts...)=$plt(vec(f);opts...)
+    @eval $plt{S<:Union(PiecewiseSpace,ArraySpace),T<:$TYP}(f::Fun{S,T},v...;opts...)=$plt(vec(f),v...;opts...)
 end
 
 
 
 ## Multivariate
 
-function contour(f::MultivariateFun;opts...)
+function contour(f::MultivariateFun,v...;opts...)
     f=chop(f,10e-10)
     f=pad(f,max(size(f,1),20),max(size(f,2),20))
     vals=values(f)
@@ -173,20 +173,20 @@ function contour(f::MultivariateFun;opts...)
         warn("Imaginary part is non-neglible.  Only plotting real part.")
     end
 
-    contour(points(space(f,1),size(vals,1)),points(space(f,2),size(vals,2)),real(vals);opts...)
+    contour(points(space(f,1),size(vals,1)),points(space(f,2),size(vals,2)),real(vals),v...;opts...)
 end
 
-contour(f::Fun;opts...)=contour(ProductFun(f);opts...)
+contour(f::Fun,v...;opts...)=contour(ProductFun(f),v...;opts...)
 
 
 
 ## 3D plotting
 # TODO: The extra vals should only be added for periodicity?
-function plot(xx::Range,yy::Range,f::MultivariateFun;opts...)
+function plot(xx::Range,yy::Range,f::MultivariateFun,v...;opts...)
     vals      = evaluate(f,xx,yy)
     #vals=[vals[:,1] vals vals[:,end]];
     #vals=[vals[1,:]; vals; vals[end,:]]
-    surf([xx],[yy],real(vals);opts...)
+    surf([xx],[yy],real(vals),v...;opts...)
 end
 
 function plot(xx::Range,yy::Range,f::MultivariateFun,obj,window)
@@ -197,11 +197,11 @@ function plot(xx::Range,yy::Range,f::MultivariateFun,obj,window)
 end
 
 
-plot(f::MultivariateFun;opts...)=surf(points(f,1),points(f,2),real(values(f));opts...)
-plot{S,V,SS<:TensorSpace}(f::ProductFun{S,V,SS};opts...)=surf(vecpoints(f,1),vecpoints(f,2),real(values(f));opts...)
-plot(f::LowRankFun;opts...)=surf(vecpoints(f,1),vecpoints(f,2),real(values(f));opts...)
+plot(f::MultivariateFun,v...;opts...)=surf(points(f,1),points(f,2),real(values(f)),v...;opts...)
+plot{S,V,SS<:TensorSpace}(f::ProductFun{S,V,SS},v...;opts...)=surf(vecpoints(f,1),vecpoints(f,2),real(values(f)),v...;opts...)
+plot(f::LowRankFun,v...;opts...)=surf(vecpoints(f,1),vecpoints(f,2),real(values(f)),v...;opts...)
 plot(f::MultivariateFun,obj,window)=glsurfupdate(real(values(f)),obj,window)
-plot{TS<:TensorSpace,T<:Real}(f::Fun{TS,T};opts...)=plot(ProductFun(f))
+plot{TS<:TensorSpace,T<:Real}(f::Fun{TS,T},v...;opts...)=plot(ProductFun(f),v...;ops...)
 
 
 # plot{S<:IntervalSpace,V<:PeriodicSpace,SS<:TensorSpace}(f::ProductFun{S,V,SS};opts...)=surf(vecpoints(f,1),vecpoints(f,2),real(values(f));opts...)
@@ -216,22 +216,22 @@ plot{TS<:TensorSpace,T<:Real}(f::Fun{TS,T};opts...)=plot(ProductFun(f))
 # end
 
 
-function plot{DS<:DiracSpace,T<:Real}(f::Fun{DS,T})
+function plot{DS<:DiracSpace,T<:Real}(f::Fun{DS,T},v...)
     n=length(space(f).points)
     plot(layer(Fun(f.coefficients[n+1:end],space(f).space)),
-               map(gadflydeltalayer,space(f).points,f.coefficients[1:n])...)
+               map(gadflydeltalayer,space(f).points,f.coefficients[1:n])...,v...)
 end
 
 ## domainplot
 
 
 for (plt,cplt) in ((:plot,:complexplot),(:layer,:complexlayer))
-    @eval $plt(d::Domain;kwds...)=$cplt(Fun(identity,d);kwds...)  # default is to call complexplot
+    @eval $plt(d::Domain,v...;kwds...)=$cplt(Fun(identity,d),v...;kwds...)  # default is to call complexplot
 end
-plot{D<:Domain}(ds::Vector{D};kwds...)=complexplot(map(d->Fun(identity,d),ds);kwds...)
+plot{D<:Domain}(ds::Vector{D},v...;kwds...)=complexplot(map(d->Fun(identity,d),ds),v...;kwds...)
 layer{D<:Domain}(d::Vector{D})=map(layer,d)
 
-domainplot(f::Union(Fun,FunctionSpace);kwds...)=plot(domain(f);kwds...)
+domainplot(f::Union(Fun,FunctionSpace),v...;kwds...)=plot(domain(f),v...;kwds...)
 domainlayer(f::Union(Fun,FunctionSpace))=layer(domain(f))
 
 
