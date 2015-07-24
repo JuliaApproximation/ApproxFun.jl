1109775ff280aaec7dfc9eb7fd4254f47cf2802f
Sheehan Olver <solver@mac.com>
2015-07-24 17:10:02 +1000

    improved example

--- examples/Dual Numbers.jl
+++ examples/Dual Numbers.jl
@@ -9,10 +9,10 @@
 using DualNumbers
 
 # We need to implement some functionality for the ApproxFun constructor to work
-Base.real{T}(::Type{Dual{T}})=Dual{ApproxFun.real(T)}
 Base.sinpi(x::Dual)=sin(π*x)
 
 using ApproxFun
+ApproxFun.real{T}(::Type{Dual{T}})=Dual{ApproxFun.real(T)}
 ApproxFun.plan_chebyshevtransform{D<:Dual}(v::Vector{D})=ApproxFun.plan_chebyshevtransform(real(v))
 ApproxFun.chebyshevtransform{D<:Dual}(v::Vector{D},plan...)=dual(chebyshevtransform(real(v),plan...),chebyshevtransform(epsilon(v),plan...))
 ApproxFun.chop!(f::Fun,d::Dual)=chop!(f,real(d))
@@ -21,5 +21,6 @@ ApproxFun.chop!(f::Fun,d::Dual)=chop!(f,real(d))
 # what is the derivative of the first coefficient with respect to the first argument of the domain?
 f=Fun(exp,Interval(dual(1.0,1),dual(2.0)))
 epsilon(f.coefficients[1])
+# check versus finite difference calculation:
 h=0.00001;(Fun(exp,Interval(1.0+h,2.0)).coefficients[1]-Fun(exp,Interval(1.0,2.0)).coefficients[1])/h
 
