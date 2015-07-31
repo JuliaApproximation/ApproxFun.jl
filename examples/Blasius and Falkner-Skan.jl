c41aabf4be68675003ee35b236a4283a0bc05f1c
cmarcotte <christopher.marcotte+git@gmail.com>
2015-07-30 23:48:04 -0400

    Comments and Corrections
    Added front commentary, and explanations throughout. Removed
    non-convergent initial estimate, and corrected boundary conditions in
    Falkner-Skan.

--- examples/Blasius and Falkner-Skan.jl
+++ examples/Blasius and Falkner-Skan.jl
@@ -1,36 +1,48 @@
-using ApproxFun        # solves 2u''' + uu'' = 0 ,  u(0) = u'(0) = 0, u'(∞) = 1
+using ApproxFun
+# The Blasius boundary layer problem (https://en.wikipedia.org/wiki/Blasius_boundary_layer)
+# describes the boundary layer on a semi-infinite plane with a steady fluid flow.
+# When properly non-dimensionalized, it presents itself as a relatively simple nonlinear ODE.
+# The Falkner-Skan problem generalizes the Blasius boundary layer by considering a wedge at an
+# angle to the steady flow.
+# In both cases, no-slip conditions are in-place at the wall (u[0.]=0,) and the boundary layer
+# asymptotically grows linearly (u'(∞)→1).
+# There are more variants which include different nonlinearities which arise in magneto-
+# hydrodynamics, for example.
+
+# We begin with Blasius:
+#   solves 2u''' + uu'' = 0 ,  u[0.] = u'[0.] = 0, u'[∞] = 1
 x=Fun(identity,[0.,4π])
 d=domain(x)
 B=[ldirichlet(d),lneumann(d),rneumann(d)]
 D=Derivative(d)
-κ = 0.33205733621519630
-u0 = (1//2) * κ * x.^2 # us = (1//2) * κ * x.^2 -(1//240)* κ^2 * x.^5 + (11//161280) * κ^3 * x.^8
-u = 0.5x^2
+κ = 0.33205733621519630     # This is diff(u,2)[0.], due to Boyd.
+u0 = (1//2) * κ * x.^2      # First-order approximation
+u = 0.5x^2                  # Other initial solutions may fail to converge
 
 f = (u)->(2.0*D^3*u + u*D^2*u)
 df = (u)->(2.0*D^3 + u*D^2 + D^2*u)
 
-ApproxFun.plot([u,u0])
+ApproxFun.plot([u,u0])      # Initial estimate and first-order approximation
 
 for k=1:10
   u  -= [B; df(u)]\[u[0.];diff(u,1)[0.];diff(u,1)[d.b]-1.;f(u)]
 end
-norm(f(u)) # should be zero
-abs(diff(u,2)[0.]-κ) # should be also zero
-ApproxFun.plot([u,u0])
-
-m = 0.10                # m ∈ [-0.0905, 2]
+norm(f(u))             # should be zero
+abs(diff(u,2)[0.]-κ)   # should also be zero
+ApproxFun.plot([u,u0]) # Solution and First-order approximation
+# Now for Falkner-Skan:
+#     solves 2u''' + uu'' - β(1 - (u')^2) = 0 ,  u[0.] = u'[0.] = 0, u'[∞] = 1
+m = 0.11                # m ∈ [-0.0905, 2]
 β = 2m/(1+m)
 F = (u)->(2.0*D^3*u + u*D^2*u - β*(1.0 - (D*u)*(D*u)))
 dF = (u)->(2.0*D^3 + u*D^2 + D^2*u + 2β*D*u*D)
 v=u
-norm(F(v)) # should be non-zero
+norm(F(v))               # should be non-zero, as we've perturbed the Blasius equation
 
 for k=1:10
-  v  -= [B; dF(v)]\[v[0.];diff(v,1);diff(v,1)[d.b]-(1.-exp(-d.b));F(v)]
+  v  -= [B; dF(v)]\[v[0.];diff(v,1)[0.];diff(v,1)[d.b]-1.;F(v)]
 end
-norm(F(v)) # should be zero
+norm(F(v))               # should be zero
 
-ApproxFun.plot([u,v]) # Blasius and Falkner-Skan solutions
+ApproxFun.plot([u,v])    # Blasius and Falkner-Skan solutions
 
-# TODO: add MHD variant with additional nonlinearity
