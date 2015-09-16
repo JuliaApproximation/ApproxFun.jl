55ddf069fc52d1041192e40068d86a6cc68a745c
Sheehan Olver <solver@mac.com>
2015-09-16 16:00:03 +1000

    resave

--- examples/Newton iteration for Nonlinear BVPs.jl
+++ examples/Newton iteration for Nonlinear BVPs.jl
@@ -1,10 +1,9 @@
 using ApproxFun
 
 x=Fun()
-d=domain(x)
+u0=0.x
 
 N=u->[u[-1.],u[1.],0.001u''+6*(1-x^2)*u'+u^2-1.]
-    @time u=newton(N,zeros(d))
-    ApproxFun.plot(u)
+u=newton(N,u0)
+ApproxFun.plot(u)
 
-u
