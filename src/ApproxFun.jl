5fe6c8aad52da0af0de434cb37c35f2373a9a38d
Sheehan Olver <solver@mac.com>
2015-09-29 16:37:42 +1000

    put inside _precompile_() function to mimic Gadfly

--- src/ApproxFun.jl
+++ src/ApproxFun.jl
@@ -41,19 +41,23 @@ include("docs.jl")
 
 
 ## precompile
-precompile(Chebyshev,tuple())
-precompile(Fun,tuple())
-precompile(Base.exp,(Fun{Chebyshev{Interval{Float64}},Float64},))
-precompile(linsolve,(Vector{Operator{Float64}},Vector{Float64}))
-precompile(\,(Vector{Operator{Float64}},Vector{Float64}))
-precompile(adaptiveqr,(Vector{Operator{Float64}},Vector{Float64},Float64,Int))
-precompile(MutableOperator,(Vector{Operator{Float64}},))
-precompile(adaptiveqr!,(MutableOperator,Vector{Float64},Float64,Int))
-precompile(+,(Int,Fun{Chebyshev{Interval{Float64}},Float64}))
-precompile(+,(Fun{Chebyshev{Interval{Float64}},Float64},Fun{Chebyshev{Interval{Float64}},Float64}))
-precompile(+,(Fun{ConstantSpace,Float64},Fun{Chebyshev{Interval{Float64}},Float64}))
-precompile(union,(ConstantSpace,Chebyshev{Interval{Float64}}))
-precompile(union,(Chebyshev{Interval{Float64}},ConstantSpace))
-precompile(Fun,(Fun{ConstantSpace,Float64},Chebyshev{Interval{Float64}}))
+function _precompile_()
+    precompile(Chebyshev,tuple())
+    precompile(Fun,tuple())
+    precompile(Base.exp,(Fun{Chebyshev{Interval{Float64}},Float64},))
+    precompile(linsolve,(Vector{Operator{Float64}},Vector{Float64}))
+    precompile(\,(Vector{Operator{Float64}},Vector{Float64}))
+    precompile(adaptiveqr,(Vector{Operator{Float64}},Vector{Float64},Float64,Int))
+    precompile(MutableOperator,(Vector{Operator{Float64}},))
+    precompile(adaptiveqr!,(MutableOperator,Vector{Float64},Float64,Int))
+    precompile(+,(Int,Fun{Chebyshev{Interval{Float64}},Float64}))
+    precompile(+,(Fun{Chebyshev{Interval{Float64}},Float64},Fun{Chebyshev{Interval{Float64}},Float64}))
+    precompile(+,(Fun{ConstantSpace,Float64},Fun{Chebyshev{Interval{Float64}},Float64}))
+    precompile(union,(ConstantSpace,Chebyshev{Interval{Float64}}))
+    precompile(union,(Chebyshev{Interval{Float64}},ConstantSpace))
+    precompile(Fun,(Fun{ConstantSpace,Float64},Chebyshev{Interval{Float64}}))
+end
+
+_precompile_()
 
 end #module
