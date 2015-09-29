161af46b0b7a248f79abeb7d1915f3c4d9bd7d1f
Sheehan Olver <solver@mac.com>
2015-09-29 15:49:34 +1000

    started precompile

--- src/ApproxFun.jl
+++ src/ApproxFun.jl
@@ -40,9 +40,14 @@ include("docs.jl")
 
 
 
-
-
-
-
+## precompile
+precompile(Chebyshev,tuple())
+precompile(Fun,tuple())
+precompile(Base.exp,(Fun{Chebyshev{Interval{Float64}},Float64},))
+precompile(linsolve,(Vector{Operator{Float64}},Vector{Float64}))
+precompile(\,(Vector{Operator{Float64}},Vector{Float64}))
+precompile(adaptiveqr,(Vector{Operator{Float64}},Vector{Float64},Float64,Int))
+precompile(MutableOperator,(Vector{Operator{Float64}},))
+precompile(adaptiveqr!,(MutableOperator,Vector{Float64},Float64,Int))
 
 end #module
