bce6d93f6e6a9902870d5f9c072432feef8ef554
Sheehan Olver <solver@mac.com>
2015-12-27 14:39:24 +1100

    remove negative orientation disk

--- src/Domains/Disk.jl
+++ src/Domains/Disk.jl
@@ -2,28 +2,26 @@ export Disk,𝔻
 
 # TT is Real or Complex
 # T is (Real,Real) or Complex
-immutable Disk{orientation,T,TT} <: BivariateDomain{T}
+immutable Disk{T,TT} <: BivariateDomain{T}
     center::TT
     radius::Float64
 end
 
-Base.convert{o}(::Type{Disk{o}},c::Complex,r)=Disk{o,typeof(c),typeof(c)}(c,r)
-Base.convert{o,T<:Real}(::Type{Disk{o}},c::Tuple{T,T},r)=Disk{o,T,typeof(c)}(c,r)
+Disk(c::Complex,r)=Disk{typeof(c),typeof(c)}(c,r)
+Disk{T<:Real}(c::Tuple{T,T},r)=Disk{T,typeof(c)}(c,r)
 
-Disk(c,r)=Disk{true}(c,r)
 Disk()=Disk((0.,0.),1.)
 Disk(::AnyDomain)=Disk(NaN,(NaN,NaN))
 
 const 𝔻=Disk()
 
 isambiguous(d::Disk)=isnan(d.radius) && all(isnan,d.center)
-Base.convert(::Type{Disk},::AnyDomain)=Disk(AnyDomain())
 
 
 #canonical is rectangle [r,0]x[-π,π]
 # we assume radius and centre are zero for now
-fromcanonical{T<:Real}(D::Disk{true,T},x,t)=D.radius*x*cos(t)+D.center[1],D.radius*x*sin(t)+D.center[2]
-tocanonical{T<:Real}(D::Disk{true,T},x,y)=sqrt((x-D.center[1])^2+(y-D.center[2]^2))/D.radius,atan2(y-D.center[2],x-D.center[1])
+fromcanonical{T<:Real}(D::Disk{T},x,t)=D.radius*x*cos(t)+D.center[1],D.radius*x*sin(t)+D.center[2]
+tocanonical{T<:Real}(D::Disk{T},x,y)=sqrt((x-D.center[1])^2+(y-D.center[2]^2))/D.radius,atan2(y-D.center[2],x-D.center[1])
 checkpoints(d::Disk)=[fromcanonical(d,(.1,.2243));fromcanonical(d,(-.212423,-.3))]
 
 # function points(d::Disk,n,m,k)
