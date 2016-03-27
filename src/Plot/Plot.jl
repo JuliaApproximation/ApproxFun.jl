86ef429ea5555cbe758798550642d593336f90ac
Sheehan Olver <solver@mac.com>
2016-03-28 09:04:04 +1100

    Restore plot for AbstractProductSpace

--- src/Plot/Plot.jl
+++ src/Plot/Plot.jl
@@ -220,7 +220,7 @@ coefficientplot!(plt::Plots.Plot,f::Fun;opts...)=plot!(plt,abs(f.coefficients);y
 
 ## Multivariate
 
-function Plots.plot!(plt::Plots.Plot,f::MultivariateFun;linetype=:contour,opts...)
+function Plots.plot!{S,V,SV<:TensorSpace}(plt::Plots.Plot,f::ProductFun{S,V,SV};linetype=:contour,opts...)
     f=chop(f,10e-10)
     f=pad(f,max(size(f,1),20),max(size(f,2),20))
     vals=values(f)
@@ -230,7 +230,6 @@ function Plots.plot!(plt::Plots.Plot,f::MultivariateFun;linetype=:contour,opts..
 
     plot!(plt,points(space(f,1),size(vals,1)),points(space(f,2),size(vals,2)),real(vals);linetype=linetype,opts...)
 end
-
 
 function Plots.surface{S<:UnivariateSpace,
                        V<:UnivariateSpace,
@@ -243,6 +242,17 @@ function Plots.surface{S<:UnivariateSpace,
     end
 
     surface(points(space(f,1),size(vals,1)),points(space(f,2),size(vals,2)),real(vals);opts...)
+end
+
+function Plots.surface(f::ProductFun;opts...)
+    f=chop(f,10e-10)
+    f=pad(f,max(size(f,1),20),max(size(f,2),20))
+    vals=values(f)
+    if norm(imag(vals),Inf)>10e-9
+        warn("Imaginary part is non-neglible.  Only plotting real part.")
+    end
+
+    surface(points(f,1),points(f,2),real(vals);opts...)
 end
 
 
@@ -272,7 +282,6 @@ for Plt in (:(Plots.plot),:(Plots.contour),:(Plots.surface))
         $Plt{TS<:AbstractProductSpace,T<:Complex}(f::Fun{TS,T};kwds...)=$Plt(ProductFun(f);kwds...)
         $Pltex{TS<:AbstractProductSpace,T<:Complex}(f::Fun{TS,T};kwds...)=$Pltex(ProductFun(f);kwds...)
         $Pltex{TS<:AbstractProductSpace,T<:Complex}(plt::Plots.Plot,f::Fun{TS,T};kwds...)=$Pltex(plt,ProductFun(f);kwds...)
-
 
         $Plt{TS<:AbstractProductSpace}(f::Fun{TS};kwds...)=$Plt(ProductFun(f);kwds...)
         $Pltex{TS<:AbstractProductSpace}(f::Fun{TS};kwds...)=$Pltex(ProductFun(f);kwds...)
