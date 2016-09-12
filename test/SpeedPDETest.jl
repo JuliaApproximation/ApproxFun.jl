c39517e90373dd39adc9451da373fc747f245e5c
Sheehan Olver <solver@mac.com>
2016-09-12 19:38:17 +1000

    update speed pde test

--- test/SpeedPDETest.jl
+++ test/SpeedPDETest.jl
@@ -5,49 +5,47 @@ using ApproxFun, Base.Test
 
 d=Interval()^2
 
-x=Fun(identity,d[1]);y=Fun(identity,d[2])
 
 #dirichlet(d) is u[-1,:],u[1,:],u[:,-1],u[:,1]
-A=[dirichlet(d);lap(d)]
-f=[real(exp(-1+1.0im*y));
-                        real(exp(1+1im*y));
-                        real(exp(x-1im));
-                        real(exp(x+1im))]
-S=schurfact(A,100)
-@time S=schurfact(A,100)
-u=S\f
-u=S\f
-@time u=S\f;
+A=[Dirichlet(d);Laplacian(d)]
+f=Fun((x,y)->real(exp(x+im*y)),∂(d))
+
+
+QR=qrfact(A)
+    ApproxFun.resizedata!(QR,:,150)
+    linsolve(QR,[f;0.];tolerance=1E-10)
+QR=qrfact(A)
+    @time ApproxFun.resizedata!(QR,:,150)
+    @time linsolve(QR,[f;0.];tolerance=1E-10)
+
 println("Laplace: should be ~0.014, 0.01")
 
 
 
 d=Interval()^2
-S=schurfact([neumann(d);lap(d)+100I],100)
-@time S=schurfact([neumann(d);lap(d)+100I],100)
-u=S\ones(4)
-u=S\ones(4)
-@time u=S\ones(4)
-println("Neumann Helmholtz: should be ~0.016, 0.016")
+[neumann(d);lap(d)+100I]\ones(4)
+@time [neumann(d);lap(d)+100I]\ones(4)
+println("Neumann Helmholtz: should be ~0.032")
 
 
 
-
-dx=Interval(0.,1.);dt=Interval(0.0,0.54)
-d=dx*dt
-
-V=Fun(x->x^2,dx)
-
-Dt=Derivative(d,[0,1]);Dx=Derivative(d,[1,0])
-
-ϵ=0.1
-
-u0=Fun(x->exp(-25*(x-.5)^2)*exp(-1.0im/(5*ϵ)*log(2cosh(5*(x-.5)))),dx)
-L=1im*ϵ*Dt+.5*ϵ^2*Dx^2-V⊗1
-
-PO=discretize([timedirichlet(d);L],50)
-@time PO=discretize([timedirichlet(d);L],50)
-u=PO\u0
-@time    u=PO\u0
-
-println("Schrodinger: should be ~0.013,0.015")
+# 
+# dx=Interval(0.,1.);dt=Interval(0.0,0.54)
+# d=dx*dt
+#
+# x,y=Fun(d)
+# V=x^2
+#
+# Dt=Derivative(d,[0,1]);Dx=Derivative(d,[1,0])
+#
+# ϵ=0.1
+#
+# u0=Fun(x->exp(-25*(x-.5)^2)*exp(-1.0im/(5*ϵ)*log(2cosh(5*(x-.5)))),dx)
+# L=1im*ϵ*Dt+.5*ϵ^2*Dx^2-V⊗1
+#
+# PO=discretize([timedirichlet(d);L],50)
+# @time PO=discretize([timedirichlet(d);L],50)
+# u=PO\u0
+# @time    u=PO\u0
+#
+# println("Schrodinger: should be ~0.013,0.015")
