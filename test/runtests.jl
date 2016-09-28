50a4b24efa2a24a0805f282da956c5633745e117
Sheehan Olver <solver@mac.com>
2016-09-29 07:19:57 +1000

    only do example tests when backend is installed

--- test/runtests.jl
+++ test/runtests.jl
@@ -43,7 +43,11 @@ include("SpeedODETest.jl")
 include("SpeedPDETest.jl")
 
 
-println("Example tests")
 
-include("ExamplesTest.jl")
-include("ReadmeTest.jl")
+println("Example tests")
+if isdir(Pkg.dir("GR")) || isdir(Pkg.dir("Plotly")) || isdir(Pkg.dir("PlotlyJS")) || isdir(Pkg.dir("PyPlot"))
+    include("ExamplesTest.jl")
+    include("ReadmeTest.jl")
+else
+    warn("Unable to do Examples since Gadfly.jl is not installed")
+end
