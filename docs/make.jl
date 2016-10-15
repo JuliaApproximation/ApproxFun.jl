0b3a9173d30efb4e03181436759da2f55aee37ae
Sheehan Olver <solver@mac.com>
2016-10-15 15:25:56 +1100

    try linux deploy docs as it seems to be passing

--- docs/make.jl
+++ docs/make.jl
@@ -6,5 +6,5 @@ deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/ApproxFun/ApproxFun.jl.git",
     julia  = "0.5",
-    osname = "osx"
+    osname = "linux"
 )
