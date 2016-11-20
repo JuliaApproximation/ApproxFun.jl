85ff3fa05e7e8513f3a99b424713ddd2ef17a239
Sheehan Olver <solver@mac.com>
2016-11-21 07:29:56 +1100

    needs to be after_script to get correct version

--- docs/travis.sh
+++ docs/travis.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ ${TRAVIS_OS_NAME} = "linux" ]; then
-    julia -e 'Pkg.add("ApproxFun")';
     julia -e 'Pkg.add("Documenter")';
     julia -e 'cd(Pkg.dir("ApproxFun")); include(joinpath("docs", "make.jl"))';
 fi
