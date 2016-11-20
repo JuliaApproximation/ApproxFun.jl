5bbb3a4bfc8670f0b35cba4f75b41da515f1950d
Sheehan Olver <solver@mac.com>
2016-11-20 21:06:18 +1100

    Fix capitalisation

--- docs/travis.sh
+++ docs/travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ${TRAVIS_OS_NAME} = "linux" ]; then
-    Julia -e 'Pkg.add("ApproxFun")';
+    julia -e 'Pkg.add("ApproxFun")';
     julia -e 'Pkg.add("Documenter")';
     julia -e 'cd(Pkg.dir("ApproxFun")); include(joinpath("docs", "make.jl"))';
 fi
