b5accbb2a257d25e3d61205fc0ef2f0153fd0f67
Sheehan Olver <solver@mac.com>
2018-07-25 08:38:08 +0100

    fix points for Int domains

--- src/Fun/Domain.jl
+++ src/Fun/Domain.jl
@@ -69,7 +69,7 @@ domainscompatible(a::Domain,b::Domain) = isambiguous(a) || isambiguous(b) ||
 ##TODO: Should fromcanonical be fromcanonical!?
 
 points(d::IntervalDomain{T},n::Integer;kind::Int=1) where {T} =
-    fromcanonical.(Ref(d), chebyshevpoints(real(eltype(T)),n;kind=kind))  # eltype to handle point
+    fromcanonical.(Ref(d), chebyshevpoints(float(real(eltype(T))), n; kind=kind))  # eltype to handle point
 bary(v::AbstractVector{Float64},d::IntervalDomain,x::Float64) = bary(v,tocanonical(d,x))
 
 #TODO consider moving these
