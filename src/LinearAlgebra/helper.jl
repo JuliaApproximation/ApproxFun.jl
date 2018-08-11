c64e173ff9a40114bb8adf5adcd1f3d4ac04ec7f
Sheehan Olver <solver@mac.com>
2018-08-11 20:25:41 +0100

    add matrix chebyshev transform

--- src/LinearAlgebra/helper.jl
+++ src/LinearAlgebra/helper.jl
@@ -1253,3 +1253,53 @@ hasnumargs(f::DFunction, k) = hasnumargs(f.f, k)
 
 dynamic(f) = f
 dynamic(f::Function) = DFunction(f) # Assume f has to compile every time
+
+
+
+## Chebyshev transform
+
+# Matrix inputs
+
+
+function FastTransforms.chebyshevtransform!(X::AbstractMatrix{T};kind::Integer=1) where T<:fftwNumber
+    if kind == 1
+        if size(X) == (1,1)
+            X
+        else
+            X=r2r!(X,REDFT10)
+            X[:,1]/=2;X[1,:]/=2;
+            scale!(1/(size(X,1)*size(X,2)),X)
+        end
+    elseif kind == 2
+        if size(X) == (1,1)
+            X
+        else
+            X=r2r!(X,REDFT00)
+            scale!(1/((size(X,1)-1)*(size(X,2)-1)),X)
+            X[:,1]/=2;X[:,end]/=2
+            X[1,:]/=2;X[end,:]/=2
+            X
+        end
+    end
+end
+
+function FastTransforms.ichebyshevtransform!(X::AbstractMatrix{T};kind::Integer=1) where T<:fftwNumber
+    if kind == 1
+        if size(X) == (1,1)
+            X
+        else
+            X[1,:]*=2;X[:,1]*=2
+            X = r2r(X,REDFT01)
+            scale!(1/4,X)
+        end
+    elseif kind == 2
+        if size(X) == (1,1)
+            X
+        else
+            X[1,:]*=2;X[end,:]*=2;X[:,1]*=2;X[:,end]*=2
+            X=chebyshevtransform!(X;kind=kind)
+            X[1,:]*=2;X[end,:]*=2;X[:,1]*=2;X[:,end]*=2
+            scale!((size(X,1)-1)*(size(X,2)-1)/4,X)
+        end
+    end
+end
