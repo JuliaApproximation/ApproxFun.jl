66ed4e34ca7b4ea23975aff0a9bd91a62c2d6d4e
Sheehan Olver <solver@mac.com>
2018-09-03 16:57:43 +0100

    Faster Rectangle points

--- src/Spaces/Chebyshev/Chebyshev.jl
+++ src/Spaces/Chebyshev/Chebyshev.jl
@@ -222,11 +222,11 @@ differentiate(f::Fun{Chebyshev{D,R}}) where {D<:Segment,R} =
 ## Multivariate
 
 function squarepoints(::Type{T}, N) where T
-    pts=paduapoints(T,Int(cld(-3+sqrt(1+8N),2)))
+    pts = paduapoints(T,Int(cld(-3+sqrt(1+8N),2)))
     n = size(pts,1)
-    ret=Array{Vec{2,T}}(undef, n)
+    ret = Array{Vec{2,T}}(undef, n)
     @inbounds for k=1:n
-        ret[k]=Vec{2,T}(pts[k,1],pts[k,2])
+        ret[k] = Vec{2,T}(pts[k,1],pts[k,2])
     end
     ret
 end
@@ -234,10 +234,11 @@ end
 function points(S::TensorSpace{Tuple{Chebyshev{D,R},Chebyshev{D,R}}},N) where {D,R}
     T = real(prectype(D))
     pts = squarepoints(T, N)
-    if domain(S) == Segment()^2
+    d = domain(S)
+    if d == Segment()^2
         pts
     else
-        fromcanonical.(Ref(S),pts)
+        pts .= fromcanonical.(Ref(d),pts)
     end
 end
 
