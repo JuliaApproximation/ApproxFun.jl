3b56f539d1baee474134bd6a7e8c02359e10d9e6
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-07-06 18:31:45 +0530

    skip broken test on v1.6

--- test/ReadmeTest.jl
+++ test/ReadmeTest.jl
@@ -110,13 +110,17 @@ using ApproxFun, SpecialFunctions, LinearAlgebra, Test
 
 
         QR = qr([Dirichlet(d);Laplacian()+100I])
-                @time ApproxFun.resizedata!(QR,:,4000)
-                @time u = \(QR,
-                                [ones(∂(d));0.];tolerance=1E-7)
-
-
-        @test u(0.1,1.) ≈ 1.0
-        @test ≈(u(0.1,0.2),-0.02768276827514463;atol=1E-8)
+        # this is broken on v1.6 (on BandedArrays < v0.16.16), so we skip on errors
+        try
+            @time ApproxFun.resizedata!(QR,:,4000)
+            @time u = \(QR, [ones(∂(d)); 0.]; tolerance=1E-7)
+            @test u(0.1,1.) ≈ 1.0
+            @test ≈(u(0.1,0.2),-0.02768276827514463;atol=1E-8)
+        catch err
+            if !(err isa MethodError && VERSION < v"1.7")
+                rethrow(err)
+            end
+        end
     end
 
     # @testset "BigFloat" begin
