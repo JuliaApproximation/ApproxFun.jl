20763becba09c9079e1e02e5afbdc9b415926aec
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-07-08 09:51:40 +0530

    limit broken test to v1.7+

--- test/ReadmeTest.jl
+++ test/ReadmeTest.jl
@@ -107,19 +107,13 @@ using ApproxFun, SpecialFunctions, LinearAlgebra, Test
         #                     [ones(∂(d));0.];tolerance=1E-10)      # First four entries of rhs are
         #
 
-
-
-        QR = qr([Dirichlet(d);Laplacian()+100I])
         # this is broken on v1.6 (on BandedArrays < v0.16.16), so we skip on errors
-        try
+        if VERSION >= v"1.7"
+            QR = qr([Dirichlet(d);Laplacian()+100I])
             @time ApproxFun.resizedata!(QR,:,4000)
             @time u = \(QR, [ones(∂(d)); 0.]; tolerance=1E-7)
             @test u(0.1,1.) ≈ 1.0
             @test ≈(u(0.1,0.2),-0.02768276827514463;atol=1E-8)
-        catch err
-            if !(err isa MethodError && VERSION < v"1.7")
-                rethrow(err)
-            end
         end
     end
 
