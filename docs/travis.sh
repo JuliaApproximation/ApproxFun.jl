921c9ca061f05fc4bbc473f3c52bb5095b62be26
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-08-08 11:35:01 +0530

    reorganize library docstrings

--- docs/travis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ ${TRAVIS_OS_NAME} = "linux" ]; then
-    julia -e 'using Pkg; Pkg.add("Documenter")';
-    julia -e 'using ApproxFun; cd(joinpath(dirname(pathof(ApproxFun)), "..", "docs")); include("make.jl")';
-fi
