c10c8856967f5b313954334bf517c9a6dc303979
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-08-24 16:24:25 +0400

    fix and improve fft methods (#804)

--- src/Extras/fftBigFloat.jl
+++ src/Extras/fftBigFloat.jl
@@ -1,17 +1,21 @@
 # This is a Cooley-Tukey FFT algorithm for any number type.
-function fft_pow2(x::Vector{F}) where F
+function fft_pow2(x::StridedVector)
     n = length(x)
-    T = mapreduce(eltype,promote_type,x)
+    T = mapreduce(eltype(x) isa Fun ? cfstype : eltype, promote_type,x)
     @assert ispow2(n)
     if n==1
-        return x
+        return convert(Vector, x)
     elseif n==2
-        return F[x[1]+x[2];x[1]-x[2]]
+        return eltype(x)[x[1]+x[2], x[1]-x[2]]
     end
-    even,odd = fft_pow2(x[1:2:end-1]),fft_pow2(x[2:2:end])
-    twiddle = exp(-2im*convert(T,π)/n*collect(0:n-1))
-    half1 = even + odd.*twiddle[1:div(n,2)]
-    half2 = even + odd.*twiddle[div(n,2)+1:n]
-    return vcat(half1,half2)
+    even,odd = fft_pow2(@view x[1:2:end-1]), fft_pow2(@view x[2:2:end])
+    ret = similar(x, n)
+    for (halfind, ind) in enumerate(1:div(n,2))
+        ret[ind] = even[halfind] + odd[halfind] * cis(-2convert(T,π)/n * (ind - 1))
+    end
+    for (halfind, ind) in enumerate(div(n,2)+1:n)
+        ret[ind] = even[halfind] + odd[halfind] * cis(-2convert(T,π)/n * (ind - 1))
+    end
+    return ret
 end
-ifft_pow2(x::Vector) = conj(fft_pow2(conj(x)))/length(x)
+ifft_pow2(x::StridedVector) = conj(fft_pow2(conj(x)))/length(x)
