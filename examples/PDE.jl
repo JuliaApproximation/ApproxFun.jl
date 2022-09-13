d6f23916de62078bab075b1142f76fdcfae9c058
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-09-14 00:05:42 +0400

    Poisson example using Literate (#822)

--- examples/PDE.jl
+++ examples/PDE.jl
@@ -1,5 +1,38 @@
 # # Solving PDEs
 
+# ## Poisson equation
+
+# We solve the Poisson equation
+# ```math
+# Δu(x,y) = 0
+# ```
+# on the rectangle `-1..1 × -1..1`,
+# with zero boundary conditions:
+
+using ApproxFun
+using LinearAlgebra
+
+d = (-1..1)^2
+x,y = Fun(d)
+f = exp.(-10(x+0.3)^2-20(y-0.2)^2)  # use broadcasting as exp(f) not implemented in 2D
+A = [Dirichlet(d); Laplacian()]
+u = A \ [zeros(∂(d)); f];
+
+# Using a QR Factorization reduces the cost of subsequent calls substantially
+
+QR = qr(A)
+u = QR \ [zeros(∂(d)); f];
+
+import Plots
+Plots.plot(u; st=:surface, legend=false)
+
+# Many PDEs have weak singularities at the corners,
+# in which case it is beneficial to specify a `tolerance` to reduce the time, as:
+
+\(A, [zeros(∂(d)); f]; tolerance=1E-6);
+
+# ## Helmholtz Equation
+
 # We solve
 # ```math
 # (Δ + 100)u(x,y) = 0
