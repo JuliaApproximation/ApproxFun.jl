a52d39f4b7b5a2db3b756c35c08560589aebc4d2
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-05-06 17:14:29 +0530

    don't re-normalize eigenfunctions in examples

--- examples/Eigenvalue.jl
+++ examples/Eigenvalue.jl
@@ -9,7 +9,7 @@ import Plots
 using LinearAlgebra: norm
 p = Plots.plot(V, legend=false, ylim=(-Inf, λ[22]))
 for k=1:20
-    Plots.plot!(real(v[k]/norm(v[k]) + λ[k]))
+    Plots.plot!(real(v[k]) + λ[k])
 end
 p
 
@@ -36,13 +36,13 @@ p1 = Plots.plot(Vfull, legend=false, ylim=(-Inf, λ[14]), title="Eigenfunctions"
     seriestype=:path, linestyle=:dash, linewidth=2)
 Plots.vline!([-Lx/2, Lx/2], color=:black)
 for k=1:12
-    Plots.plot!(real(v[k]/norm(v[k]) + λ[k]))
+    Plots.plot!(real(v[k]) + λ[k])
 end
 
 # Zoom into the ground state:
 p2 = Plots.plot(Vfull, legend=false, ylim=(-Inf, λ[3]), title="Ground state",
     seriestype=:path, linestyle=:dash, linewidth=2)
 Plots.vline!([-Lx/2, Lx/2], color=:black)
-Plots.plot!(real(v[1]/norm(v[1]) + λ[1]), linewidth=2)
+Plots.plot!(real(v[1]) + λ[1], linewidth=2)
 
 Plots.plot(p1, p2, layout = 2)
