67d0cad86880b0b892fb0bce7193e7e37715dae8
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-06-01 15:51:11 +0530

    Move `DualNumbers` to an extension (#888)
    * Move DualNumbers to an extension
    
    * only define dualcfsFun in ApproxFun
    
    * fix path

--- src/Extras/Extras.jl
+++ src/Extras/Extras.jl
@@ -20,6 +20,10 @@ include("simplify.jl")
 include("autodifferentiation.jl")
 include("fractional.jl")
 
+function dualFun end
+function dualcfsFun end
 
-include("dualnumbers.jl")
+if !isdefined(Base, :get_extension)
+	include(joinpath(dirname(dirname(pathof(@__MODULE__))), "ext", "ApproxFunDualNumbersExt.jl"))
+end
 include("lanczos.jl")
