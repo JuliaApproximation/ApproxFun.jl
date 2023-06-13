0ef226531dab5f85b2177dac5bf2861eeb4b0875
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-06-13 15:13:44 +0530

    re-enable test for bigfloat

--- test/NumberTypeTest.jl
+++ test/NumberTypeTest.jl
@@ -55,21 +55,19 @@ include(joinpath(@__DIR__, "testutils.jl"))
     end
 
     @testset "BigFloat relative tolerance bug test Issue #557" begin
-		if false # disabled due to Julia issue https://github.com/JuliaLang/julia/issues/25853
-	        x = Fun(BigFloat(0)..BigFloat(20_000));
-	        ν = BigFloat(1568)
-	        f = x^(ν/2-1) * exp(-x/2)
-	        # TODO: JacobiWeight should support general types to avoid warning here
-	        @test f(10.0) ≈ 10.0^(ν/2-1) * exp(-10.0/2) rtol=eps()
-	        ex_mathematica = big"8.6494114955713766301430915207861966687081153e778"
-	        @test (cumsum(f)(10.0) - ex_mathematica)/ex_mathematica ≤ eps()
+        x = Fun(BigFloat(0)..BigFloat(20_000));
+        ν = BigFloat(1568)
+        f = x^(ν/2-1) * exp(-x/2)
+        # TODO: JacobiWeight should support general types to avoid warning here
+        @test f(10.0) ≈ 10.0^(ν/2-1) * exp(-10.0/2) rtol=eps()
+        ex_mathematica = big"8.6494114955713766301430915207861966687081153e778"
+        @test (cumsum(f)(10.0) - ex_mathematica)/ex_mathematica ≤ eps()
 
-	        x = Fun(BigFloat(0)..BigFloat(Inf))
-	        ν = BigFloat(1568)
-            f = x^(ν/2-1) * exp(-x/2)
-            @test f(10.0) ≈ 10.0^(ν/2-1) * exp(-10.0/2) rtol=eps()
-            @test_skip (cumsum(f)(10.0) - ex_mathematica)/ex_mathematica ≤ eps()
-        end
+        x = Fun(Ray(BigFloat(0)..BigFloat(Inf)))
+        ν = BigFloat(1568)
+        f = x^(ν/2-1) * exp(-x/2)
+        @test f(10.0) ≈ 10.0^(ν/2-1) * exp(-10.0/2) rtol=eps()
+        @test_skip (cumsum(f)(10.0) - ex_mathematica)/ex_mathematica ≤ eps()
     end
 
     @testset "#641" begin
